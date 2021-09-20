6c006ee21b054fa50920ebed1b0f2b43015593d3
apexpilatus <apexpilatus@gmail.com>
2021-09-20 22:29:35 +0300

    update

--- main.c
+++ main.c
@@ -11,13 +11,10 @@
 #include <alsa/pcm.h>
 #include <alsa/error.h>
 
-
-const int fuck = 8;
-
 const int time_out = 3;
-const char play_file_path[] = "/home/player/play";
+const char play_file_path[] = "/home/disk/player/play";
 const char music_root[] = "/home/music";
-const char album_file_path[] = "/home/player/album";
+const char album_file_path[] = "/home/disk/player/album";
 
 void write_0_to_play_file() {
 	FILE *play_file;
@@ -62,16 +59,20 @@ int main() {
 				write_0_to_play_file();
 				continue;
 			}
-			snd_pcm_hw_params_t *pcm_hw = malloc(snd_pcm_hw_params_sizeof());
+			snd_pcm_hw_params_t *pcm_hw;
+			if (snd_pcm_hw_params_malloc(&pcm_hw)){
+				snd_pcm_close(pcm_p);
+				write_0_to_play_file();
+				continue;
+			}
 			snd_pcm_hw_params_any(pcm_p, pcm_hw);
-
 			snd_pcm_hw_params_set_access(pcm_p, pcm_hw,
 					SND_PCM_ACCESS_MMAP_INTERLEAVED);
 			unsigned int rate = 44100;
 			int dir = -1;
 			snd_pcm_hw_params_set_rate_near(pcm_p, pcm_hw, &rate, &dir);
-			if (snd_pcm_hw_params(pcm_p, pcm_hw)) {
-				free(pcm_hw);
+			if (snd_pcm_hw_params(pcm_p, pcm_hw) || snd_pcm_prepare(pcm_p)) {
+				snd_pcm_hw_params_free(pcm_hw);
 				snd_pcm_close(pcm_p);
 				write_0_to_play_file();
 				continue;
@@ -79,12 +80,10 @@ int main() {
 
 			snd_pcm_hw_params_get_buffer_size(pcm_hw,
 					(snd_pcm_uframes_t*) &buf_size);
-			free(pcm_hw);
+			snd_pcm_hw_params_free(pcm_hw);
 
 			unsigned int buf[buf_size];
-
 			unsigned long read_size = 0;
-
 			char album_val[1024];
 			get_album(album_val);
 			for (int i = 1; i < 100; i++) {
