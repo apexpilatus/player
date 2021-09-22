b36a8fc0a1759e0710921e5f78204306487aa2d5
apexpilatus <apexpilatus@gmail.com>
2021-09-22 14:08:21 +0300

    add java

---
+++ java/src/main/java/Lister.java
@@ -0,0 +1,27 @@
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Lister extends HttpServlet {
+    File musicDir = new File("/home/music");
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (musicDir.exists() && musicDir.isDirectory()) {
+            String[] albums = musicDir.list();
+            Arrays.sort(albums);
+            if (Objects.requireNonNull(albums).length != 0) {
+                resp.setContentType("text/html");
+                for (String album:albums){
+                    resp.getWriter().println("<p style=\"font-size:3em\"><a href=http://72.12.120.1:8080/player?album=" + album.replace(" ","%20") + ">" + album + "</a></p>");
+                }
+                resp.getWriter().println("<p style=\"font-size:3em\">_ _ _ _ _ _ _</p>");
+            }
+        }
+    }
+}
---
+++ java/src/main/java/Player.java
@@ -0,0 +1,30 @@
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Player extends HttpServlet {
+    String playFilePath = "/home/player/play";
+    String albumFilePath = "/home/player/album";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        String album = req.getParameter("album");
+        if (album != null) {
+            try (
+                    BufferedWriter playFileWriter = new BufferedWriter(new FileWriter(new File(playFilePath)));
+                    BufferedWriter albumFileWriter = new BufferedWriter(new FileWriter(new File(albumFilePath)));
+            ) {
+                playFileWriter.write(1);
+                albumFileWriter.write(album);
+                resp.sendRedirect("http://72.12.120.1:8080/lister");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
