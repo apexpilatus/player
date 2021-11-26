eba06237a2d9cf400c6e0dbeec95b9413f2b940a
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2021-11-26 22:03:29 +0300

    Update Lister.java

--- java/src/main/java/Lister.java
+++ java/src/main/java/Lister.java
@@ -9,17 +9,17 @@ import java.util.Objects;
 
 public class Lister extends HttpServlet {
     String musicPath = "/home/disk/music";
-    File musicDir = new File(musicPath);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        File musicDir = new File(musicPath);
         if (musicDir.exists() && musicDir.isDirectory()) {
             String[] albums = musicDir.list();
             Arrays.sort(albums);
             if (Objects.requireNonNull(albums).length != 0) {
                 resp.setContentType("text/html");
                 for (String album:albums){
-                    resp.getWriter().println("<p style=\"font-size:2em\"><a href=http://jetson:8080/player?album=" + album.replace(" ","%20") + ">" + album + "</a></p>");
+                    resp.getWriter().println("<p style=\"font-size:2em\"><a href=http://jetson:8080/player?album=" + musicPath + "/" + album.replace(" ","%20") + ">" + album + "</a></p>");
                 }
                 resp.getWriter().println("<p style=\"font-size:3em\">_ _ _ _ _ _ _</p>");
             }
