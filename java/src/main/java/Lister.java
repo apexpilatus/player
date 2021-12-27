30209df5b3a9b1764556babd385ccd63b718f534
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2021-12-27 23:13:45 +0300

    Update Lister.java

--- java/src/main/java/Lister.java
+++ java/src/main/java/Lister.java
@@ -19,7 +19,7 @@ public class Lister extends HttpServlet {
             if (Objects.requireNonNull(albums).length != 0) {
                 resp.setContentType("text/html");
                 for (String album:albums){
-                    resp.getWriter().println("<p style=\"font-size:1em\"><a href=http://jetson:8080/player?album=" + musicPath + "/" + album.replace(" ","%20") + ">" + album + "</a></p>");
+                    resp.getWriter().println("<p style=\"font-size:1em\"><a href=http://jetson:8080/player?album=" + musicPath + "/" + album.replace(" ","%20") + ">" + album.replace("fuckingslash","/") + "</a></p>");
                 }
                 resp.getWriter().println("<p style=\"font-size:1em\">_ _ _ _ _ _ _</p>");
             }
