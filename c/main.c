639512f0a06b1e225936007cb2120d98f886f46b
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-01-09 21:06:13 +0300

    Update main.c

--- c/main.c
+++ c/main.c
@@ -16,13 +16,12 @@ int main(int argsn, char *args[]) {
 	unsigned long buf_size_in_frames;
 	if (snd_pcm_open(&pcm_p, "hw:2,0", SND_PCM_STREAM_PLAYBACK, 0)) {
 		write_0_to_play_file();
-		execl(exec_waiter_path, "play.waiter", NULL);
+		execl(exec_waiter_path, "play.waiter", "cannot open pcm", NULL);
 	}
 	snd_pcm_hw_params_t *pcm_hw;
 	if (snd_pcm_hw_params_malloc(&pcm_hw)){
-		snd_pcm_close(pcm_p);
 		write_0_to_play_file();
-		execl(exec_waiter_path, "play.waiter", NULL);
+		execl(exec_waiter_path, "play.waiter", "cannot alocate memory for hw params", NULL);
 	}
 	snd_pcm_hw_params_any(pcm_p, pcm_hw);
 	snd_pcm_hw_params_set_access(pcm_p, pcm_hw, SND_PCM_ACCESS_MMAP_INTERLEAVED);
@@ -30,10 +29,8 @@ int main(int argsn, char *args[]) {
 	snd_pcm_hw_params_set_rate_near(pcm_p, pcm_hw, &rate, &dir);
 	snd_pcm_hw_params_set_format(pcm_p, pcm_hw, frame_size == 4 ? SND_PCM_FORMAT_S16_LE : SND_PCM_FORMAT_S24_3LE);
 	if (snd_pcm_hw_params(pcm_p, pcm_hw) || snd_pcm_prepare(pcm_p)) {
-		snd_pcm_hw_params_free(pcm_hw);
-		snd_pcm_close(pcm_p);
 		write_0_to_play_file();
-		execl(exec_waiter_path, "play.waiter", NULL);
+		execl(exec_waiter_path, "play.waiter", "cannot start playing", NULL);
 	}
 	snd_pcm_hw_params_get_buffer_size(pcm_hw, (snd_pcm_uframes_t*) &buf_size_in_frames);
 	snd_pcm_hw_params_free(pcm_hw);
@@ -58,7 +55,7 @@ int main(int argsn, char *args[]) {
 				if ((play_err = snd_pcm_mmap_writei(pcm_p, buf, (snd_pcm_uframes_t) buf_size_in_frames)) < 0) {
 					close(music_file_dstr);
 					write_0_to_play_file();
-					execl(exec_waiter_path, "play.waiter", NULL);
+					execl(exec_waiter_path, "play.waiter", "play error", NULL);
 				}
 			}
 			while ((read_size = read(music_file_dstr, buf, buf_size_in_bytes))) {
@@ -75,10 +72,10 @@ int main(int argsn, char *args[]) {
 			close(music_file_dstr);
 			if (play_err < 0) {
 				write_0_to_play_file();
-				execl(exec_waiter_path, "play.waiter", NULL);
+				execl(exec_waiter_path, "play.waiter", "play error", NULL);
 			}
 			if (check_album(args[1]) != 0) {
-				execl(exec_waiter_path, "play.waiter", NULL);
+				execl(exec_waiter_path, "play.waiter", "new album", NULL);
 			}
 		} else {
 			if (read_size < buf_size_in_bytes && read_size > 0) {
@@ -88,7 +85,7 @@ int main(int argsn, char *args[]) {
 				}
 			}
 			write_0_to_play_file();
-			execl(exec_waiter_path, "play.waiter", NULL);
+			execl(exec_waiter_path, "play.waiter", "the end", NULL);
 		}
 	}
 }
