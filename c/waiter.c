80ac4c1f6fd5b4ac21d6f8d21ea371fa1aad993e
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-07 18:39:24 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -14,7 +14,7 @@ int main(int argsn, char *args[]){
 			if (!snd_pcm_open(&pcm_p, "hw:2,0", SND_PCM_STREAM_PLAYBACK, 0)) {
 				snd_pcm_info_t * pcm_info = malloc(snd_pcm_info_sizeof());
 				snd_pcm_info(pcm_p, pcm_info);
-				execl(exec_play_path, "play.waiter", snd_pcm_info_get_subdevice_name(pcm_info), album_val, NULL);
+				execl(exec_play_path, "play.waiter", snd_pcm_info_get_id(pcm_info), album_val, NULL);
 			}
 			
 			execl(exec_play_path, "play.waiter", "fook", album_val, NULL);
