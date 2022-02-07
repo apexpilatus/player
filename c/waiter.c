329c765163931021a1c8dca3278f147c381d5f26
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-07 17:36:21 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -9,6 +9,13 @@ int main(int argsn, char *args[]){
 		} else {
 			char album_val[1024];
 			get_album(album_val);
+			
+			snd_ctl_t *pcm_c;
+			if (snd_ctl_open (&pcm_c, "hw:2,0", SND_CTL_NONBLOCK)) {
+				//execl(exec_waiter_path, "play.waiter", "cannot open pcm", NULL);
+				execl(exec_play_path, "play.waiter", album_val, snd_ctl_name(pcm_c), NULL);
+			}
+			
 			execl(exec_play_path, "play.waiter", album_val, NULL);
 		}
 	}
