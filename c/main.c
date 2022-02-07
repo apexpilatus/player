068383ffa7dedec8e60048c42d3f36c702c9ad3d
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-07 22:26:45 +0300

    Update main.c

--- c/main.c
+++ c/main.c
@@ -3,7 +3,7 @@
 #endif
 
 int main(int argsn, char *args[]) {
-	int rate = atoi(args[2]);
+	int rate = atoi(args[2]), frame_size = atoi(args[3]);
 	snd_pcm_t *pcm_p;
 	unsigned long buf_size_in_frames;
 	if (snd_pcm_open(&pcm_p, args[1], SND_PCM_STREAM_PLAYBACK, 0)) {
@@ -20,7 +20,7 @@ int main(int argsn, char *args[]) {
 	snd_pcm_hw_params_set_access(pcm_p, pcm_hw, SND_PCM_ACCESS_MMAP_INTERLEAVED);
 	int dir = -1;
 	snd_pcm_hw_params_set_rate_near(pcm_p, pcm_hw, &rate, &dir);
-	snd_pcm_hw_params_set_format(pcm_p, pcm_hw, memcmp(args[3], "USB Audi", 8) ? SND_PCM_FORMAT_S16_LE : SND_PCM_FORMAT_S24_3LE);
+	snd_pcm_hw_params_set_format(pcm_p, pcm_hw, frame_size == 4 ? SND_PCM_FORMAT_S16_LE : SND_PCM_FORMAT_S24_3LE);
 	if (snd_pcm_hw_params(pcm_p, pcm_hw) || snd_pcm_prepare(pcm_p)) {
 		write_0_to_play_file();
 		snd_pcm_close(pcm_p);
