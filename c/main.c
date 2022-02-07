e3e7220ca7e06537ece81d735cb18ba8a935a796
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-07 21:17:19 +0300

    Update main.c

--- c/main.c
+++ c/main.c
@@ -21,7 +21,7 @@ int main(int argsn, char *args[]) {
 	snd_pcm_hw_params_any(pcm_p, pcm_hw);
 	snd_pcm_hw_params_set_access(pcm_p, pcm_hw, SND_PCM_ACCESS_MMAP_INTERLEAVED);
 	int dir = -1;
-	snd_pcm_hw_params_set_rate_near(pcm_p, pcm_hw, &rate, &dir);
+	snd_pcm_hw_params_set_rate_near(pcm_p, pcm_hw, atoi(args[2]), &dir);
 	snd_pcm_hw_params_set_format(pcm_p, pcm_hw, frame_size == 4 ? SND_PCM_FORMAT_S16_LE : SND_PCM_FORMAT_S24_3LE);
 	if (snd_pcm_hw_params(pcm_p, pcm_hw) || snd_pcm_prepare(pcm_p)) {
 		write_0_to_play_file();
