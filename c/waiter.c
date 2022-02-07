13a61981cf81bcd495ae038337b3bd04b079fe4f
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-07 19:53:42 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -12,9 +12,9 @@ int main(int argsn, char *args[]){
 			
 			snd_ctl_t *ctl_p;
 			char **card_name;
-			if (!snd_ctl_open(&ctl_p, "hw:2", SND_CTL_ASYNC)) {
+			if (!(snd_ctl_open(&ctl_p, "hw:2", SND_CTL_ASYNC) || snd_card_get_longname(2, card_name))) {
 				snd_card_get_name()
-				execl(exec_play_path, "play.waiter", snd_ctl_name(ctl_p), album_val, NULL);
+				execl(exec_play_path, "play.waiter", *card_name, album_val, NULL);
 			}
 			
 			execl(exec_play_path, "play.waiter", "fook", album_val, NULL);
