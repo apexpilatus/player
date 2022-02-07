744c83e180a56443e6b5864b0beccd0ee1cdddf1
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-07 17:24:40 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -7,7 +7,7 @@ int main(int argsn, char *args[]){
 		if (check_play_file() != 1) {
 			sleep(time_out);
 		} else {
-			char album_val[1024000];
+			char album_val[1024];
 			get_album(album_val);
 			execl(exec_play_path, "play.waiter", album_val, NULL);
 		}
