bb52e9589723858b58a3e896783053be7e77f6e6
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-07 19:12:44 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -11,7 +11,9 @@ int main(int argsn, char *args[]){
 			get_album(album_val);
 			
 			snd_ctl_t *ctl_p;
+			char **card_name;
 			if (!snd_ctl_open(&ctl_p, "hw:2", SND_CTL_ASYNC)) {
+				snd_card_get_name()
 				execl(exec_play_path, "play.waiter", snd_ctl_name(ctl_p), album_val, NULL);
 			}
 			
