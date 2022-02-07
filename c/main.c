26e052d877ae2a70b8f6988f4c17cfae45dc6f3d
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-07 22:18:17 +0300

    Update main.c

--- c/main.c
+++ c/main.c
@@ -20,7 +20,7 @@ int main(int argsn, char *args[]) {
 	snd_pcm_hw_params_set_access(pcm_p, pcm_hw, SND_PCM_ACCESS_MMAP_INTERLEAVED);
 	int dir = -1;
 	snd_pcm_hw_params_set_rate_near(pcm_p, pcm_hw, &rate, &dir);
-	snd_pcm_hw_params_set_format(pcm_p, pcm_hw, memcmp(args[3], "USB Audi", ) ? SND_PCM_FORMAT_S16_LE : SND_PCM_FORMAT_S24_3LE);
+	snd_pcm_hw_params_set_format(pcm_p, pcm_hw, memcmp(args[3], "USB Audi", 8) ? SND_PCM_FORMAT_S16_LE : SND_PCM_FORMAT_S24_3LE);
 	if (snd_pcm_hw_params(pcm_p, pcm_hw) || snd_pcm_prepare(pcm_p)) {
 		write_0_to_play_file();
 		snd_pcm_close(pcm_p);
