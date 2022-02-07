6c299b3ad359efff796aafdcdd641cd437887908
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-07 22:06:37 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -12,8 +12,8 @@ int main(int argsn, char *args[]){
 			unsigned int rate;
 			unsigned short frame_size;
 			get_params(album_val, &rate, &frame_size);
-			char rate_as_str[10];
-			sprintf(rate_as_str, "%d\0", rate);
+			char rate_as_str[100];
+			sprintf(rate_as_str, "%d", rate);
 			char **card_name;
 			for (int i=0; i<6; i++) {
 				if (!snd_card_get_name(i, card_name) && !memcmp(*card_name, frame_size == 4 ? "irDAC II" : "USB Audi", 8)) {
