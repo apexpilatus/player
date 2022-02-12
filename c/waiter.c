96197479dd840596ebaf30c24779e2cf088b9e49
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-12 21:46:38 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -37,12 +37,20 @@ int main(int argsn, char *args[]){
 					char **check_name;
 					if (!snd_card_get_name(card_num, check_name) && !strcmp(*check_name, "USB Audio 24bit 96khz")){
 						for (int i=0; i<ecount; i++){
-							if (!strcmp(snd_ctl_elem_list_get_name(elist, i)+13, "Switch")){
-								printf("fuck\n");
+							if (!strcmp(snd_ctl_elem_list_get_name(elist, i)+13, "Switch")) {
+								snd_ctl_elem_value_t *eval;
+								snd_ctl_elem_value_malloc(&eval);
+								snd_ctl_elem_id_t *e_id;
+								snd_ctl_elem_id_malloc(&e_id);
+								snd_ctl_elem_list_get_id(elist, i, e_id);
+								snd_ctl_elem_value_set_id(eval, e_id);
+								snd_ctl_elem_value_set_numid(eval, snd_ctl_elem_list_get_numid(elist, i));
+								snd_ctl_elem_read(ctl_p, eval));
+								snd_ctl_elem_value_set_boolean(eval, 0, 0);
+								snd_ctl_elem_write(ctl_p, eval));
 							}
 						}
 					}
-					
 					snd_ctl_close(ctl_p);
 				}
 				
