1dc9a8f9e37dcc980eca7c3f219a7319ec745928
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-20 01:48:12 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -45,10 +45,10 @@ int main(int argsn, char *args[]){
 					printf("%s\n", *check_name);
 					snd_card_get_name(card_num, check_name);
 					printf("%s\n", *check_name);
-					/*
+					
 					if (!snd_card_get_name(card_num, check_name) && !strcmp(*check_name, "USB Audio 24bit 96khz")){
 						printf("fiik\n");
-						
+						/*
 						for (int i=0; i<ecount; i++){
 							if (!strcmp(snd_ctl_elem_list_get_name(elist, i)+13, "Switch")) {
 								snd_ctl_elem_value_t *eval;
@@ -75,8 +75,8 @@ int main(int argsn, char *args[]){
 								snd_ctl_elem_value_set_integer(eval, 1, 110);
 								snd_ctl_elem_write(ctl_p, eval);
 							}
-						}
-					}*/
+						}*/
+					}
 					snd_ctl_close(ctl_p);
 				}
 				sprintf(card_name, "hw:%d,0", card_num);
