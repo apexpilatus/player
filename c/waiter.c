b2ebecf46846aa60a4c6a6a80a91900bac5f5661
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-02-20 00:59:19 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -32,7 +32,7 @@ int main(int argsn, char *args[]){
 				
 				snd_ctl_t *ctl_p;
 				if (!snd_ctl_open(&ctl_p, card_name, SND_CTL_NONBLOCK)){
-					snd_ctl_nonblock(ctl_p, 0);
+					//snd_ctl_nonblock(ctl_p, 0);
 					snd_ctl_elem_list_t *elist;
 					snd_ctl_elem_list_malloc(&elist);
 					snd_ctl_elem_list(ctl_p, elist);
