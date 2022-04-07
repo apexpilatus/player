2568ccf176a5b24070907f649d4f7b09164ddc58
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-04-07 16:35:15 +0300

    Update funcs.h

--- c/funcs.h
+++ c/funcs.h
@@ -24,7 +24,7 @@ typedef struct lst{
   struct lst *next;
 } file_lst;
 
-extern *file_lst get_file_lst(char *dir);
+extern file_lst* get_file_lst(char *dir);
 extern void write_0_to_play_file();
 extern char check_play_file();
 extern void get_album(char *ret);
