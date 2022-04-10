2108de9167f16d4e08076615c6597bea977be12b
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-04-10 21:22:47 +0300

    Update funcs.c

--- c/funcs.c
+++ c/funcs.c
@@ -122,7 +122,8 @@ FLAC__StreamDecoderWriteStatus write_callback(const FLAC__StreamDecoder *decoder
 	}
 	int framesize=snd_pcm_hw_params_get_sbits(pcm_hw)/4;
 	snd_pcm_hw_params_free(pcm_hw);
-	buf_size_in_bites;
+	buf_size_in_bytes = framesize*2*frame->header.blocksize;
+	buf_size_in_frames = buf_size_in_bytes/framesize;
 	char * playbuf = malloc(framesize*2*frame->header.blocksize);
 	for(i = 0; i < frame->header.blocksize; i++) {
 		cp_little_endian(playbuf+(i*framesize*2), buffer[0][i], framesize);
