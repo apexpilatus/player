8450ced31e35a0efb1bffe410edaa7bedc6822c7
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-04-11 23:06:13 +0300

    Update main.c

--- c/main.c
+++ c/main.c
@@ -7,18 +7,18 @@ int main(int argsn, char *args[]) {
 	snd_pcm_t *pcm_p;
 	FLAC__StreamDecoder *decoder = NULL;
 	if((decoder = FLAC__stream_decoder_new()) == NULL) {
-		write_0_to_play_file();
+		write_0_to_album_file();
 		execl(exec_waiter_path, "play.waiter", "cannot allocate decoder", NULL);
 	}
 	(void)FLAC__stream_decoder_set_md5_checking(decoder, true);
 	(void)FLAC__stream_decoder_set_metadata_ignore_all(decoder);
 	if (snd_pcm_open(&pcm_p, args[1], SND_PCM_STREAM_PLAYBACK, 0)) {
-		write_0_to_play_file();
+		write_0_to_album_file();
 		execl(exec_waiter_path, "play.waiter", "cannot open pcm", NULL);
 	}
 	snd_pcm_hw_params_t *pcm_hw;
 	if (snd_pcm_hw_params_malloc(&pcm_hw)){
-		write_0_to_play_file();
+		write_0_to_album_file();
 		snd_pcm_close(pcm_p);
 		FLAC__stream_decoder_delete(decoder);
 		execl(exec_waiter_path, "play.waiter", "cannot allocate memory for hw params", NULL);
@@ -29,7 +29,7 @@ int main(int argsn, char *args[]) {
 	snd_pcm_hw_params_set_rate_near(pcm_p, pcm_hw, &rate, &dir);
 	snd_pcm_hw_params_set_format(pcm_p, pcm_hw, frame_size == 4 ? SND_PCM_FORMAT_S16_LE : SND_PCM_FORMAT_S24_3LE);
 	if (snd_pcm_hw_params(pcm_p, pcm_hw) || snd_pcm_prepare(pcm_p)) {
-		write_0_to_play_file();
+		write_0_to_album_file();
 		snd_pcm_close(pcm_p);
 		FLAC__stream_decoder_delete(decoder);
 		execl(exec_waiter_path, "play.waiter", "cannot start playing", NULL);
@@ -43,7 +43,7 @@ int main(int argsn, char *args[]) {
 		init_status = FLAC__stream_decoder_init_file(decoder, file_name, write_callback, metadata_callback, error_callback, pcm_p);
 		if(init_status == FLAC__STREAM_DECODER_INIT_STATUS_OK) {
 			if (!FLAC__stream_decoder_process_until_end_of_stream(decoder)){
-				write_0_to_play_file();
+				write_0_to_album_file();
 				snd_pcm_close(pcm_p);
 				FLAC__StreamDecoderState dec_state = FLAC__stream_decoder_get_state(decoder);
 				FLAC__stream_decoder_finish(decoder);
@@ -52,7 +52,7 @@ int main(int argsn, char *args[]) {
 			}
 			FLAC__stream_decoder_finish(decoder);
 		} else {
-			write_0_to_play_file();
+			write_0_to_album_file();
 			snd_pcm_close(pcm_p);
 			FLAC__stream_decoder_delete(decoder);
 			execl(exec_waiter_path, "play.waiter", "cannot init file", files->name, FLAC__StreamDecoderInitStatusString[init_status], NULL);
@@ -60,7 +60,7 @@ int main(int argsn, char *args[]) {
 		files=files->next;
 	}
 	snd_pcm_drain(pcm_p);
-	write_0_to_play_file();
+	write_0_to_album_file();
 	snd_pcm_close(pcm_p);
 	FLAC__stream_decoder_delete(decoder);
 	execl(exec_waiter_path, "play.waiter", "the end", NULL);
