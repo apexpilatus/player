867af1989b2ac838d9a9ae29c4f7496771f81ad6
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-04-11 22:55:18 +0300

    Update funcs.c

--- c/funcs.c
+++ c/funcs.c
@@ -2,7 +2,7 @@
 	#include "funcs.h"
 #endif
 
-void write_0_to_play_file() {
+void write_0_to_album_file() {
 	int play_file_dstr;
 	while ((play_file_dstr = open(album_file_path, O_NONBLOCK|O_WRONLY)) == -1) {
 		sleep(time_out);
@@ -12,7 +12,7 @@ void write_0_to_play_file() {
 	close(play_file_dstr);
 }
 
-char check_play_file(){
+char check_play_val(){
         int play_file_dstr;
         while ((play_file_dstr = open(album_file_path, O_NONBLOCK|O_RDONLY)) == -1) {
                 sleep(time_out);
