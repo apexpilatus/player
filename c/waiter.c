c9ee5419b9a3ee1fee4f566e3f19cc85d33d11dd
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-04-11 23:45:26 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -10,22 +10,22 @@ int main(int argsn, char *args[]){
 			char album_val[1024];
 			get_album(album_val);
 			unsigned int rate;
-			unsigned short frame_size;
+			unsigned short sample_size;
 			file_lst *files=get_file_lst(album_val);
 			if (!files->next && !files->name){
 				write_0_to_album_file();
 				execl(exec_waiter_path, "play.waiter", "directory is empty", NULL);
 			}
-			if (get_params(album_val, files, &rate, &frame_size)){
+			if (get_params(album_val, files, &rate, &sample_size)){
 				write_0_to_album_file();
 				execl(exec_waiter_path, "play.waiter", "files have different format or cannot read", files->name, NULL);
 			}
-			char rate_as_str[6], frame_size_as_str[3];
+			char rate_as_str[6], sample_size_as_str[3];
 			snprintf(rate_as_str, 6, "%d", rate);
-			snprintf(frame_size_as_str, 3, "%d", frame_size);
-			int card_num = snd_card_get_index(frame_size == 24 ? "U96khz" : "II");
+			snprintf(sample_size_as_str, 3, "%d", sample_size);
+			int card_num = snd_card_get_index(sample_size == 24 ? "U96khz" : "II");
 			if (card_num >= 0){
-				if (frame_size == 24) {
+				if (sample_size == 24) {
 					char card_name[5];
 					snprintf(card_name, 5, "hw:%d", card_num);
 					snd_ctl_t *ctl_p;
@@ -69,7 +69,7 @@ int main(int argsn, char *args[]){
 				}
 				char card_pcm_name[7];
 				snprintf(card_pcm_name, 7, "hw:%d,0", card_num);
-				execl(exec_play_path, "play.waiter", card_pcm_name, rate_as_str, frame_size_as_str, album_val, NULL);
+				execl(exec_play_path, "play.waiter", card_pcm_name, rate_as_str, sample_size_as_str, album_val, NULL);
 			}
 			write_0_to_album_file();
 			execl(exec_waiter_path, "play.waiter", "no card to play", NULL);
