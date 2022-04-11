c7e0f8ebd0283fda397f6768b5f70cd1b2e62835
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-04-12 01:34:04 +0300

    Update main.c

--- c/main.c
+++ c/main.c
@@ -9,7 +9,10 @@ int main(int argsn, char *args[]) {
 	char fuck[1024];
 	get_album(fuck);
 	write_0_to_album_file();
-		execl(exec_waiter_path, "play.waiter", fuck, getenv("ALBM"), NULL);
+	int cmcm=strcmp(fuck, getenv("ALBM"));
+	char cmcmstr[1024];
+	sprintf(cmcmstr,"%d",cmcm);
+		execl(exec_waiter_path, "play.waiter", fuck, getenv("ALBM"), cmcmstr, NULL);
 	
 	int rate = atoi(args[2]), sample_size = atoi(args[3]);
 	snd_pcm_t *pcm_p;
