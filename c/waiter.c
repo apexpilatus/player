e90fd19576c955514664e29810d1d7c66c02683a
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-04-12 00:38:11 +0300

    Update waiter.c

--- c/waiter.c
+++ c/waiter.c
@@ -70,7 +70,7 @@ int main(int argsn, char *args[]){
 				char card_pcm_name[7];
 				snprintf(card_pcm_name, 7, "hw:%d,0", card_num);
 				char *env[] = {malloc(1050), (char *) NULL};
-				strcpy(env[0], "BUBU=");
+				strcpy(env[0], "ALBM=");
 				strcpy(env[0]+5, album_val);
 				execle(exec_play_path, "play.waiter", card_pcm_name, rate_as_str, sample_size_as_str, album_val, (char *) NULL, env);
 			}
