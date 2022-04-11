77e9bfbb8dbaa1d2b17c69f0aae472a2c5491828
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-04-12 01:43:01 +0300

    Update funcs.c

--- c/funcs.c
+++ c/funcs.c
@@ -116,7 +116,7 @@ void cp_little_endian(char *buf, FLAC__uint32 data, int samplesize)
 }
 
 FLAC__StreamDecoderWriteStatus write_callback(const FLAC__StreamDecoder *decoder, const FLAC__Frame *frame, const FLAC__int32 * const buffer[], void *client_data){
-	if (check_album){
+	if (check_album()){
 		return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
 	}
 	snd_pcm_t *pcm_p = (snd_pcm_t*)client_data;
