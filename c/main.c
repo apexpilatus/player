9fc6236461b3d5e6557a1b28b9f1673822a4e289
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-04-12 01:16:58 +0300

    Update main.c

--- c/main.c
+++ c/main.c
@@ -5,10 +5,10 @@
 int main(int argsn, char *args[]) {
 	
 	
-			write_0_to_album_file();
+			//write_0_to_album_file();
 	//char fuck[1024];
 	//get_album(fuck);
-		execl(exec_waiter_path, "fuck", NULL);
+		//execl(exec_waiter_path, "fuck", NULL);
 	
 	int rate = atoi(args[2]), sample_size = atoi(args[3]);
 	snd_pcm_t *pcm_p;
