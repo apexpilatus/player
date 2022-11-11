2e858f6e5bb447d05cc976b509886d6722998b81
apexpilatus <apexpilatus@gmail.com>
2022-11-11 10:40:38 +0400

    add music dirs

--- java/src/main/java/Player.java
+++ java/src/main/java/Player.java
@@ -7,12 +7,10 @@ import org.jflac.metadata.Picture;
 
 import java.io.*;
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Objects;
+import java.util.*;
 
 public class Player extends HttpServlet {
-    String musicDirPath = "/home/store/music";
+    String[] musicDirPaths = {"/home/store/music", "/home/disk/music"};
     String exeDirPath = "/home/exe";
     String albumFilePath = exeDirPath + "/player/tmp/album";
     String volumeFilePath = exeDirPath + "/player/tmp/volume";
@@ -48,8 +46,16 @@ public class Player extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        File musicDirFile = new File(musicDirPath);
-        if (musicDirFile.exists()) {
+        Map<String, String> albums = new TreeMap<>();
+        for (String musicDirPath : musicDirPaths) {
+            File musicDir = new File(musicDirPath);
+            if (musicDir.exists()) {
+                for (String album : Objects.requireNonNull(musicDir.list())) {
+                    albums.put(album, musicDirPath);
+                }
+            }
+        }
+        if (!albums.isEmpty()) {
             String pictureDirPath = exeDirPath + "/player/tmp";
             String pictureName = "picture.jpeg";
             File dirFile = new File(exeDirPath);
@@ -94,35 +100,39 @@ public class Player extends HttpServlet {
                     e.printStackTrace();
                 }
             }
-            String[] albums = musicDirFile.list();
-            Arrays.sort(Objects.requireNonNull(albums));
-            if (Objects.requireNonNull(albums).length != 0) {
-                resp.setContentType("text/html");
+            resp.setContentType("text/html");
+            try {
+                resp.getWriter().println("<head><meta charset=UTF-8><title>player</title><link rel=apple-touch-icon href=apple-touch-icon.png type=image/png></head>");
+                resp.getWriter().println("<body style=background-color:gray>");
+                resp.getWriter().println("<iframe name=vol height=45px width=50px style=border:none;position:fixed;top:40px;right:60px;></iframe>");
+                resp.getWriter().println("<form action=http://" + req.getHeader("Host") +
+                        "/player?volume=up method=post target=vol style=position:fixed;top:40px;left:20px;>");
+                resp.getWriter().println("<input type=submit value=up>");
+                resp.getWriter().println("</form>");
+                resp.getWriter().println("<form action=http://" + req.getHeader("Host") +
+                        "/player?volume=down method=post target=vol style=position:fixed;top:100px;left:20px;>");
+                resp.getWriter().println("<input type=submit value=dw>");
+                resp.getWriter().println("</form>");
+                resp.getWriter().println("<img src=http://" + req.getHeader("Host") +
+                        "/" + pictureName + " style=width:200px;height:200px;position:fixed;top:250px;left:20px;>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            albums.forEach((album, albumPath) -> {
                 try {
-                    resp.getWriter().println("<head><meta charset=UTF-8><title>player</title><link rel=apple-touch-icon href=apple-touch-icon.png type=image/png></head>");
-                    resp.getWriter().println("<body style=background-color:gray>");
-                    resp.getWriter().println("<iframe name=vol height=45px width=50px style=border:none;position:fixed;top:40px;right:60px;></iframe>");
-                    resp.getWriter().println("<form action=http://" + req.getHeader("Host") +
-                            "/player?volume=up method=post target=vol style=position:fixed;top:40px;left:20px;>");
-                    resp.getWriter().println("<input type=submit value=up>");
-                    resp.getWriter().println("</form>");
-                    resp.getWriter().println("<form action=http://" + req.getHeader("Host") +
-                            "/player?volume=down method=post target=vol style=position:fixed;top:100px;left:20px;>");
-                    resp.getWriter().println("<input type=submit value=dw>");
-                    resp.getWriter().println("</form>");
-                    resp.getWriter().println("<img src=http://" + req.getHeader("Host") +
-                            "/" + pictureName + " style=width:200px;height:200px;position:fixed;top:250px;left:20px;>");
-                    for (String album : albums) {
-                        resp.getWriter().println("<p style=text-align:center;color:white;font-size:150%><a href=http://" + req.getHeader("Host") +
-                                "/player?album=" + musicDirPath + "/" + album.replace(" ", "%20") + ">" +
-                                album.replace("fuckingslash", "/").replace("fuckingblackstar", "&#9733").replace(" anD ", " & ").replace("___", "</a> <b><small>") +
-                                "</a></b></small></p>");
-                    }
-                    resp.getWriter().println("<p style=font-size:1em;text-align:center>_ _ _ _ _ _ _</p>");
-                    resp.getWriter().println("</body>");
+                    resp.getWriter().println("<p style=text-align:center;color:white;font-size:150%><a href=http://" + req.getHeader("Host") +
+                            "/player?album=" + albumPath + "/" + album.replace(" ", "%20") + ">" +
+                            album.replace("fuckingslash", "/").replace("fuckingblackstar", "&#9733").replace(" anD ", " & ").replace("___", "</a> <b><small>") +
+                            "</a></b></small></p>");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            });
+            try {
+                resp.getWriter().println("<p style=font-size:1em;text-align:center>_ _ _ _ _ _ _</p>");
+                resp.getWriter().println("</body>");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
