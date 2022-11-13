ee01809bdbe1e1ca336c7a647864f3e20a211e1a
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-11-13 12:02:32 +0400

    Update Player.java

--- java/src/main/java/Player.java
+++ java/src/main/java/Player.java
@@ -101,7 +101,7 @@ public class Player extends HttpServlet {
                 resp.getWriter().println("<img id=picture src=" + pictureName + " style=width:200px;height:200px;position:fixed;top:300px;left:20px;>");
 
                 resp.getWriter().println("<iframe id=tracks src=data:text/html,huhu style=position:fixed;top:300px;right:20px;></iframe>");
-                resp.getWriter().println("<button type=button onclick=gettracks(\"fuck\") style=border-radius:50%;color:white;background-color:black;font-size:20px;position:fixed;top:100px;right:20px;>fk</button>");
+                resp.getWriter().println("<button type=button onclick=gettracks(\"" + "/home/store/music/ACfuckingslashDC___The Razors Edge".replace(" ", "&") + "\") style=border-radius:50%;color:white;background-color:black;font-size:20px;position:fixed;top:100px;right:20px;>fk</button>");
 
             } catch (IOException e) {
                 e.printStackTrace();
