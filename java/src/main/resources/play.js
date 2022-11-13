700747dc55cff8e6c1fd3f95d2426bd49af5283e
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2022-11-13 10:14:14 +0400

    Update play.js

--- java/src/main/resources/play.js
+++ java/src/main/resources/play.js
@@ -15,3 +15,12 @@ function volume(direction){
 	xhttp.open("POST", window.location.href.replace("player", "control") + "?volume=" + direction);
 	xhttp.send();
 }
+
+function tracks(album){
+	const xhttp = new XMLHttpRequest();
+	xhttp.onload = function() {
+		document.getElementById("tracks").innerHTML = this.responseText;
+	}
+	xhttp.open("POST", window.location.href.replace("player", "control") + "?album=" + album.replace(/&/g, " "));
+	xhttp.send();
+}
