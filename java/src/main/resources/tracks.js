5a4ae16d4708841f62c04dc4848b98c68ba64f98
apexpilatus <apexpilatus@gmail.com>
2022-11-15 06:27:14 +0400

    hide tracks only if track == 1

--- java/src/main/resources/tracks.js
+++ java/src/main/resources/tracks.js
@@ -2,7 +2,9 @@ function play(album, track){
 	const xhttp = new XMLHttpRequest();
 	xhttp.onload = function() {
 		parent.document.getElementById("picture").src = this.responseText;
-		parent.document.getElementById("tracks").hidden = true;
+		if(track == "01.flac"){
+			parent.document.getElementById("tracks").hidden = true;
+		}
 	}
 	xhttp.open("POST", parent.window.location.href + "?album=" + album.replace(/&/g, " ") + "&track=" + track);
 	xhttp.send();
