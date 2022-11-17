dfc363abdcf04f291830eeda341950a97f357231
apexpilatus <apexpilatus@gmail.com>
2022-11-17 10:52:01 +0400

    tiny

--- java/src/main/java/Player.java
+++ java/src/main/java/Player.java
@@ -123,9 +123,9 @@ public class Player extends HttpServlet {
             resp.getWriter().println("<body style=background-color:gray>");
             resp.getWriter().println("<script src=main.js></script>");
             resp.getWriter().println("<script>setvolume(\"keep\")</script>");
-            resp.getWriter().println("<p id=volume style=position:fixed;top:50px;left:20px;>&#127911</p>");
-            resp.getWriter().println("<button type=button onclick=setvolume(\"up\") style=border-radius:20%;color:black;background-color:white;font-size:20px;position:fixed;top:150px;left:20px;>up</button>");
-            resp.getWriter().println("<button type=button onclick=setvolume(\"down\") style=border-radius:20%;color:black;background-color:white;font-size:20px;position:fixed;top:200px;left:20px;>dw</button>");
+            resp.getWriter().println("<p id=volume style=position:fixed;top:80px;left:20px;font-size:20px;></p>");
+            resp.getWriter().println("<button type=button onclick=setvolume(\"up\") style=border-radius:20px;color:black;background-color:white;font-size:20px;position:fixed;top:50px;left:20px;>up</button>");
+            resp.getWriter().println("<button type=button onclick=setvolume(\"down\") style=border-radius:20px;color:black;background-color:white;font-size:20px;position:fixed;top:150px;left:20px;>dw</button>");
             resp.getWriter().println("<img id=picture src=" + pictureName + " style=width:250px;height:250px;position:fixed;top:300px;left:20px;>");
             resp.getWriter().println("<img hidden id=picturebytes style=width:100px;height:100px;position:fixed;top:10px;right:20px;>");
             resp.getWriter().println("<iframe hidden id=tracks width=400 height=430 style=position:fixed;top:120px;right:20px;></iframe>");
