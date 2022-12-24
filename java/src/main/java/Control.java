69e5113f7126cfd866e8942c7dacac7c2d7cde4b
apexpilatus <apexpilatus@gmail.com>
2022-12-24 14:47:12 +0400

    tiny

--- java/src/main/java/Control.java
+++ java/src/main/java/Control.java
@@ -126,7 +126,7 @@ public class Control extends HttpServlet {
                             title += "<p style=color:black;font-size:120%;><b>" + vorbisArtist[0] + "</b><br><strong style=color:white;>" + vorbisAlbum[0] + "</strong></p>\n";
                             title += "</body>";
                         }
-                        htmlFileWriter.write("<i onclick=play(\"" + albumToList.replace(" ", "&") + "\",\"" + file + "\")>" + vorbisTrack[0] + vorbisTitle[0] + "</i><br>\n");
+                        htmlFileWriter.write("<i onclick=play(\"" + albumToList.replace(" ", "&") + "\",\"" + file + "\")><small style=color:white;>" + vorbisTrack[0] + "</small>" + vorbisTitle[0] + "</i><br>\n");
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
