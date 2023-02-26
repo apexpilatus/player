b8082d48a5625a516bf2edb698f46aa1c22feeb5
Maxim Otrishko <maxim.otrishko@thommen.aero>
2023-02-26 16:20:45 +0100

    mv some

--- spring/src/main/java/MainPage.java
+++ spring/src/main/java/MainPage.java
@@ -1,5 +1,3 @@
-package controllers;
-
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- spring/src/main/java/starter/MainStarter.java
+++ spring/src/main/java/starter/MainStarter.java
@@ -1,6 +1,6 @@
 package starter;
 
-import controllers.MainPage;
+import MainPage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
