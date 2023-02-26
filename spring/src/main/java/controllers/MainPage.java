e13c3242cb544514af8da56dcde8cbf598f0709d
Maxim Otrishko <maxim.otrishko@thommen.aero>
2023-02-26 16:25:27 +0100

    mv

--- spring/src/main/java/controllers/MainPage.java
+++ spring/src/main/java/controllers/MainPage.java
@@ -1,3 +1,5 @@
+package controllers;
+
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- spring/src/main/java/starter/MainStarter.java
+++ spring/src/main/java/starter/MainStarter.java
@@ -1,12 +1,9 @@
 package starter;
 
-import MainPage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(MainPage.class)
 public class MainStarter {
     public static void main(String[] args) {
         SpringApplication.run(MainStarter.class, args);
