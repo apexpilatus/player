cfb7dde72c0bd3df7b9523cdbb4cda2dd8290238
Maxim Otrishko <maxim.otrishko@thommen.aero>
2023-02-26 16:29:25 +0100

    import

--- spring/src/main/java/starter/MainStarter.java
+++ spring/src/main/java/starter/MainStarter.java
@@ -1,9 +1,12 @@
 package starter;
 
+import controllers.MainPage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(MainPage.class)
 public class MainStarter {
     public static void main(String[] args) {
         SpringApplication.run(MainStarter.class, args);
