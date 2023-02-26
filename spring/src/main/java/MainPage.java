2001024d624adb332f3c24e2dffd3616d5a35100
Maxim Otrishko <maxim.otrishko@thommen.aero>
2023-02-26 17:29:58 +0100

    rm favicon from html

--- spring/src/main/java/MainPage.java
+++ spring/src/main/java/MainPage.java
@@ -10,7 +10,6 @@ public class MainPage {
     void mainPage(HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         resp.getWriter().println("<head><meta charset=UTF-8><title>player</title>" +
-                "<link rel=icon type=image/x-icon href=favicon.ico>" +
                 "<link rel=apple-touch-icon href=apple-180x180.png sizes=180x180 type=image/png>" +
                 "</head>");
         resp.getWriter().write("fuck");
