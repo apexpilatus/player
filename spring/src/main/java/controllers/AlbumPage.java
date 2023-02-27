dcb3a51da4e87325fc142bdb5782490263df27d4
Maxim Otrishko <maxim.otrishko@thommen.aero>
2023-02-27 14:22:49 +0100

    print album

--- spring/src/main/java/controllers/AlbumPage.java
+++ spring/src/main/java/controllers/AlbumPage.java
@@ -2,6 +2,7 @@ package controllers;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -9,8 +10,8 @@ import java.io.IOException;
 @RestController
 public class AlbumPage {
     @GetMapping("/album")
-    void albumPage(HttpServletResponse resp) throws IOException {
+    void albumPage(@RequestParam("album") String album, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
-        resp.getWriter().println("fuck");
+        resp.getWriter().println(album);
     }
 }
