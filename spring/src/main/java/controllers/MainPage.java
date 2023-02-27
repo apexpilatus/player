b33e1a1abba3d4c779f4aca0801da62391de2933
Maxim Otrishko <maxim.otrishko@thommen.aero>
2023-02-27 21:49:21 +0100

    tiny

--- spring/src/main/java/controllers/MainPage.java
+++ spring/src/main/java/controllers/MainPage.java
@@ -58,7 +58,7 @@ public class MainPage {
         respWriter.println("<body style=background-color:slategray;>");
         respWriter.println("<p id=volume style=position:fixed;top:80px;left:20px;font-size:20px;></p>");
         respWriter.println("<button type=button onclick=setVolume(\"up\") style=border-radius:20px;color:black;background-color:white;font-size:20px;position:fixed;top:50px;left:10px;>up</button>");
-        respWriter.println("<button type=button onclick=setVolume(\"down\") style=border-radius:20px;color:black;background-color:white;font-size:20px;position:fixed;top:4600px;left:10px;>dw</button>");
+        respWriter.println("<button type=button onclick=setVolume(\"down\") style=border-radius:20px;color:black;background-color:white;font-size:20px;position:fixed;top:460px;left:10px;>dw</button>");
         respWriter.println("<script>setVolume(\"init\")</script>");
         respWriter.println("<iframe hidden id=tracks width=450 height=430 style=\"position:fixed;top:10px;right:10px;border-style:solid;\"></iframe>");
         respWriter.println("<img hidden id=trackspicture style=width:250px;height:250px;position:fixed;top:10px;right:470px;border-style:solid;>");
