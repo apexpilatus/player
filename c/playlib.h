0851d34761844cf3ab6dc4ffdbde64df6dc6e057
apexpilatus <apexpilatus@gmail.com>
2023-03-16 07:53:46 +0100

    refactor

--- c/playlib.h
+++ c/playlib.h
@@ -2,8 +2,9 @@ typedef struct lst{
   char *name;
   struct lst *next;
 } file_lst;
+typedef FLAC__StreamDecoderWriteStatus (*wr_cb_func)(const FLAC__StreamDecoder *, const FLAC__Frame *, const FLAC__int32 * const [], void *);
 
-int play_album(file_lst *files, FLAC__StreamDecoderWriteStatus (*write_callback)(const FLAC__StreamDecoder *decoder, const FLAC__Frame *frame, const FLAC__int32 * const buffer[], void *client_data), snd_pcm_t *pcm_p);
+int play_album(file_lst *files, wr_cb_func write_callback, snd_pcm_t *pcm_p);
 void cp_little_endian(unsigned char *buf, FLAC__uint32 data, int samplesize);
 file_lst* get_file_lst(char *dirname);
 int get_params(file_lst *files, unsigned int *rate, unsigned short *sample_size);
