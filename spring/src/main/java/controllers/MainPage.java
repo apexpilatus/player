c56b079eebf2ede02f0fd7a8609bb40d861849f5
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2023-05-01 16:23:23 +0200

    Update MainPage.java

--- spring/src/main/java/controllers/MainPage.java
+++ spring/src/main/java/controllers/MainPage.java
@@ -27,15 +27,15 @@ public class MainPage {
                 respWriter.println("</head>");
                 respWriter.println("<body style=background-color:slategray;>");
                 respWriter.println(
-                                "<button id=showvolume onclick=getVolume() style=border-radius:20%;color:red;background-color:black;font-size:25px;position:fixed;top:460px;right:120px;><b>&#9738</b></button>");
+                                "<button id=showvolume onclick=getVolume() style=border-radius:20%;color:red;background-color:black;font-size:25px;position:fixed;top:430px;right:120px;><b>&#9738</b></button>");
                 respWriter.println(
                                 "<input hidden id=volume type=range onchange=setVolume() min=0 max=5 style=position:fixed;top:400px;right:50px;width:500px>");
                 respWriter.println(
-                                "<iframe hidden id=tracks width=450 height=350 style=\"position:fixed;top:10px;right:10px;border-style:solid;\"></iframe>");
+                                "<iframe hidden id=tracks width=450 height=400 style=\"position:fixed;top:10px;right:10px;border-style:solid;\"></iframe>");
                 respWriter.println(
                                 "<img hidden id=trackspicture style=width:320px;height:320px;position:fixed;top:10px;left:10px;border-style:solid;>");
                 respWriter.println(
-                                "<button hidden id=hidetracks onclick=hideTracks() style=border-radius:20%;color:red;background-color:black;font-size:25px;position:fixed;top:460px;right:10px;>X</button>");
+                                "<button hidden id=hidetracks onclick=hideTracks() style=border-radius:20%;color:red;background-color:black;font-size:25px;position:fixed;top:430px;right:10px;>X</button>");
                 respWriter.println(
                                 "<ul style=padding-left:50px;font-size:150%;line-height:180%;list-style-type:circle;>");
                 albums.forEach((album, albumPathList) -> albumPathList.forEach((albumPath) -> respWriter.println(
