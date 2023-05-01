ec75a98005b5efe5ddc55967c1478de5b9c13e41
Maxim Otrishko <maxim.otrishko@advalange.com>
2023-05-01 18:06:08 +0200

    tiny

--- spring/src/main/resources/public/main.js
+++ spring/src/main/resources/public/main.js
@@ -2,7 +2,7 @@ function play(album) {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
         hideTracks();
-        document.getElementById("albums").hidden = false;
+        document.getElementById("albums").class = list;
     }
     xhttp.open("GET", window.location.href + "play?album=" + album.replace(/&/g, " "));
     xhttp.send();
@@ -11,7 +11,7 @@ function play(album) {
 function getTracks(album) {
     document.getElementById("tracks").src = window.location.href + "album?album=" + album.replace(/&/g, " ");
     document.getElementById("volume").hidden = true;
-    document.getElementById("albums").hidden = true;
+    document.getElementById("albums").class = hidden;
     document.getElementById("tracks").hidden = false;
     document.getElementById("trackspicture").hidden = false;
     document.getElementById("hidetracks").hidden = false;
