1e6a96e2d45873e31cf0339575f42ca82b4abe36
Maxim Otrishko <maxim.otrishko@advalange.com>
2023-05-04 17:54:20 +0200

    ffkk

--- spring/src/main/java/configuration/Config.java
+++ spring/src/main/java/configuration/Config.java
@@ -1,8 +1,5 @@
 package configuration;
 
-import java.io.File;
-import java.time.Duration;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -14,11 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Config implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        File ffkk = new File(".");
-        System.out.println(ffkk.list()[0]);
-        System.out.println(System.getProperties());
-        registry.addResourceHandler("/*")
-                .addResourceLocations("ServletContext:/")
-                .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
+        registry.addResourceHandler("/*").addResourceLocations("ServletContext:/")
+                .setCacheControl(CacheControl.noCache());
     }
 }
