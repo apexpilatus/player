4b843da8fdc3ee0779ecfa95978e7520d65d6d3a
Maxim Otrishko <maxim.otrishko@advalange.com>
2023-05-04 16:34:38 +0200

    wtf

--- spring/src/main/java/configuration/Config.java
+++ spring/src/main/java/configuration/Config.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Config implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/public")
+        registry.addResourceHandler("/resources/**").addResourceLocations("/public", "classpath:/static/")
                 .setCacheControl(CacheControl.noCache());
     }
 }
