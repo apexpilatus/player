aefc1bc0e92d02f8d49ddadd19e4fa23ce9be91e
Maxim Otrishko <maxim.otrishko@advalange.com>
2023-05-04 17:49:37 +0200

    ffkk

--- spring/src/main/java/starter/MainStarter.java
+++ spring/src/main/java/starter/MainStarter.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({ MainPage.class, Volume.class, AlbumPage.class, Play.class})
+@Import({ MainPage.class, Volume.class, AlbumPage.class, Play.class, Config.class })
 public class MainStarter {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(MainStarter.class);
