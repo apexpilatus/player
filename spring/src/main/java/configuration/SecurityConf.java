c101b9c50f7a603634a024398d01f622e4838d0d
Maxim Otrishko <maxim.otrishko@advalange.com>
2023-05-04 19:49:00 +0200

    ffkk

--- spring/src/main/java/configuration/SecurityConf.java
+++ spring/src/main/java/configuration/SecurityConf.java
@@ -8,7 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class SecurityConf {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
--- spring/src/main/java/starter/MainStarter.java
+++ spring/src/main/java/starter/MainStarter.java
@@ -4,13 +4,14 @@ import controllers.AlbumPage;
 import controllers.MainPage;
 import controllers.Play;
 import controllers.Volume;
+import configuration.SecurityConf;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({ MainPage.class, Volume.class, AlbumPage.class, Play.class })
+@Import({ MainPage.class, Volume.class, AlbumPage.class, Play.class, SecurityConf.class })
 public class MainStarter {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(MainStarter.class);
