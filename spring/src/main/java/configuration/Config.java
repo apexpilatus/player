e56effe52a75be74521560527013da0216ddc6b1
Maxim Otrishko <maxim.otrishko@advalange.com>
2023-05-04 16:07:05 +0200

    tiny

--- spring/src/main/java/configuration/Config.java
+++ spring/src/main/java/configuration/Config.java
@@ -11,6 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Config implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("*.css").setCacheControl(CacheControl.noCache());
+        registry.addResourceHandler("*.css").addResourceLocations("/").setCacheControl(CacheControl.noCache());
     }
 }
