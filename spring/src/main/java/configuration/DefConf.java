f54635246ea30e63a273fcf57e75e5d521ba33ba
Maxim Otrishko <maxim.otrishko@advalange.com>
2023-05-04 20:56:58 +0200

    ffkk

--- spring/src/main/java/configuration/DefConf.java
+++ spring/src/main/java/configuration/DefConf.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class DefConf implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("**").addResourceLocations("classpath:public/")
+        registry.addResourceHandler("/*").addResourceLocations("classpath:public/")
                 .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
     }
 }
