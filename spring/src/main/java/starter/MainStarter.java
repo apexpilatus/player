474af0de13d6bf7ec84922a9a14df8be632cca94
Maxim Otrishko <maxim.otrishko@advalange.com>
2023-05-04 20:47:30 +0200

    remove security

--- spring/src/main/java/starter/MainStarter.java
+++ spring/src/main/java/starter/MainStarter.java
@@ -4,14 +4,13 @@ import controllers.AlbumPage;
 import controllers.MainPage;
 import controllers.Play;
 import controllers.Volume;
-import configuration.SecurityConf;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({ MainPage.class, Volume.class, AlbumPage.class, Play.class, SecurityConf.class })
+@Import({ MainPage.class, Volume.class, AlbumPage.class, Play.class, })
 public class MainStarter {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(MainStarter.class);
