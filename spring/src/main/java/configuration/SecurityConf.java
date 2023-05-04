7ba11f46343755ea021072e677c50c3c621aa2f7
Maxim Otrishko <maxim.otrishko@advalange.com>
2023-05-04 20:02:58 +0200

    use login\passwd

--- spring/src/main/java/configuration/SecurityConf.java
+++ spring/src/main/java/configuration/SecurityConf.java
@@ -2,17 +2,20 @@ package configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConf {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.headers(headers -> headers.cacheControl().disable());
-        return http.build();
-    }
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(
+				User.withDefaultPasswordEncoder().username("maxotra").password("aragupegu").roles("USER").build());
+		return manager;
+	}
 }
