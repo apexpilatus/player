db723b48c9f6b788df00cf306fed30aada747ea7
Maxim Otrishko <maxim.otrishko@advalange.com>
2023-05-04 20:14:46 +0200

    x-frame -> sameorigin

--- spring/src/main/java/configuration/SecurityConf.java
+++ spring/src/main/java/configuration/SecurityConf.java
@@ -2,10 +2,12 @@ package configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -17,5 +19,11 @@ public class SecurityConf {
 		manager.createUser(
 				User.withDefaultPasswordEncoder().username("maxotra").password("aragupegu").roles("USER").build());
 		return manager;
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+		return http.build();
 	}
 }
