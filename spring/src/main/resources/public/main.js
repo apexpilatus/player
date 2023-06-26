aabf4f9bb508fc61d11f3eacb8a9cec4c6171469
apexpilatus <apexpilatus@gmail.com>
2023-06-26 22:24:55 +0200

    tiny

--- spring/src/main/resources/public/main.js
+++ spring/src/main/resources/public/main.js
@@ -9,16 +9,23 @@ function play(album) {
 
 function getTracks(album) {
     document.getElementById("tracks").src = window.location.href + "album?album=" + album.replace(/&/g, " ");
-    document.getElementById("volume").hidden = true;
+    document.getElementById("showvolume").hidden = true;
     document.getElementById("albums").setAttribute("class", "unscroll");
 }
 
 function hideTracks() {
-    document.getElementById("tracks").src = window.location.href + "volume";
+    document.getElementById("tracks").src = "data:text/plain,ok";
     document.getElementById("albums").setAttribute("class", "scroll");
     document.getElementById("tracks").hidden = true;
     document.getElementById("trackspicture").hidden = true;
     document.getElementById("hidetracks").hidden = true;
+    document.getElementById("showvolume").hidden = false;
+}
+
+function hideVolume() {
+    document.getElementById("volume").hidden = true;
+    document.getElementById("showvolume").hidden = false;
+    document.getElementById("albums").setAttribute("class", "scroll");
 }
 
 function getVolume() {
@@ -28,17 +35,14 @@ function getVolume() {
         if (resp.split(";")[1] == 0) {
             document.getElementById("showvolume").innerHTML = "&#9739";
         } else {
-            hideTracks();
+            document.getElementById("albums").setAttribute("class", "unscroll");
             document.getElementById("showvolume").innerHTML = "&#9738";
             document.getElementById("volume").min = 0;
             document.getElementById("volume").max = resp.split(";")[1];
             document.getElementById("volume").value = resp.split(";")[0];
             document.getElementById("volume").hidden = false;
             document.getElementById("showvolume").hidden = true;
-            delayVar = setTimeout(function () {
-                document.getElementById("volume").hidden = true;
-                document.getElementById("showvolume").hidden = false;
-            }, 4000);
+            delayVar = setTimeout(hideVolume, 4000);
         }
     }
     xhttp.open("GET", window.location.href + "volume");
@@ -49,10 +53,7 @@ function setVolume() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
         clearTimeout(delayVar);
-        delayVar = setTimeout(function () {
-            document.getElementById("volume").hidden = true;
-            document.getElementById("showvolume").hidden = false;
-        }, 4000);
+        delayVar = setTimeout(hideVolume, 4000);
     }
     xhttp.open("POST", window.location.href + "volume?level=" + document.getElementById("volume").value);
     xhttp.send();
