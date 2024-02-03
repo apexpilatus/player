829f006feb76ffa4519a7ef99a0287636dc06f55
Max <apexpilatus@gmail.com>
2024-02-03 20:41:01 +0100

    creating webserver on C

---
+++ web/page_main.c
@@ -0,0 +1,70 @@
+#include <dirent.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void list_albums(char *msg) {
+  char src_path[4096];
+  DIR *dp_music;
+  struct dirent *src_ep;
+  dp_music = opendir(music);
+  if (dp_music) {
+    strcat(msg, "<dl>");
+    while ((src_ep = readdir(dp_music))) {
+      if (src_ep->d_type == DT_DIR && strcmp(src_ep->d_name, ".") &&
+          strcmp(src_ep->d_name, "..") &&
+          strcmp(src_ep->d_name, "lost+found")) {
+        DIR *dp_src;
+        struct dirent *albm_ep;
+        sprintf(src_path, "%s/%s", music, src_ep->d_name);
+        dp_src = opendir(src_path);
+        if (dp_src) {
+          while ((albm_ep = readdir(dp_src))) {
+            if (albm_ep->d_type == DT_DIR && strcmp(albm_ep->d_name, ".") &&
+                strcmp(albm_ep->d_name, "..") &&
+                strcmp(albm_ep->d_name, "lost+found")) {
+              strcat(msg, "<dt>");
+              strcat(msg, albm_ep->d_name);
+              strcat(msg, "</dt>");
+            }
+          }
+          closedir(dp_src);
+        }
+      }
+    }
+    strcat(msg, "</dl>");
+    closedir(dp_music);
+  }
+}
+
+int main(int prm_n, char *prm[]) {
+  int sock = strtol(prm[1], NULL, 10);
+  ssize_t msg_size = 4096, write_size;
+  char rsp[msg_size], *msg;
+  msg = malloc(msg_size * 10000);
+  strcpy(msg, "<!DOCTYPE html>");
+  strcat(msg, "<head>");
+  strcat(msg, "<meta charset=utf-8>");
+  strcat(msg,
+         "<meta name=viewport content=width=device-width, initial-scale=1.0>");
+  strcat(msg, "<title>player</title>");
+  strcat(msg, "</head>");
+  strcat(msg, "<body>");
+  list_albums(msg);
+  strcat(msg, "</body>");
+  strcat(msg, "</html>");
+  strcpy(rsp, "HTTP/1.1 200 OK\r\n");
+  strcat(rsp, "Content-Type: text/html; charset=utf-8\r\n");
+  write_size = strlen(rsp);
+  sprintf(rsp + write_size, "Content-Length: %lu\r\n\r\n", strlen(msg));
+  write_size = write(sock, rsp, strlen(rsp));
+  write_size += write(sock, msg, strlen(msg));
+  kill(getppid(), SIGUSR1);
+  if (write_size == strlen(rsp) + strlen(msg)) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
---
+++ web/web.c
@@ -0,0 +1,90 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define listen_port 8080
+
+typedef struct pid_lst_t {
+  pid_t pid;
+  int sock;
+  struct pid_lst_t *prev;
+  struct pid_lst_t *next;
+} pid_lst;
+
+pid_lst *pids_first = NULL, *pids_last = NULL;
+
+void kill_zombie(int signum) {
+  pid_t pid = wait(NULL);
+  pid_lst *pid_tmp = pids_first;
+  while (pid_tmp) {
+    if (pid_tmp->pid == pid) {
+      if (pid_tmp->prev) {
+        pid_tmp->prev->next = pid_tmp->next;
+      } else {
+        pids_first = pid_tmp->next;
+      }
+      if (pid_tmp->next) {
+        pid_tmp->next->prev = pid_tmp->prev;
+      } else {
+        pids_last = pid_tmp->prev;
+      }
+      close(pid_tmp->sock);
+      free(pid_tmp);
+      break;
+    }
+    pid_tmp = pid_tmp->next;
+  }
+}
+
+int main(void) {
+  sigset_t block_alarm;
+  pid_t pid;
+  char arg[12];
+  int sock_listen, sock;
+  struct sockaddr_in addr;
+
+  sigemptyset(&block_alarm);
+  sigaddset(&block_alarm, SIGUSR1);
+  signal(SIGUSR1, kill_zombie);
+  sock_listen = socket(PF_INET, SOCK_STREAM, 0);
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(listen_port);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  socklen_t addr_size = sizeof(addr);
+  if (bind(sock_listen, (struct sockaddr *)&addr, addr_size) < 0) {
+    return 1;
+  }
+  if (listen(sock_listen, 10) < 0) {
+    return 1;
+  }
+  while (1) {
+    sock = accept(sock_listen, (struct sockaddr *)&addr, &addr_size);
+    if (sock < 0) {
+      continue;
+    }
+    sigprocmask(SIG_BLOCK, &block_alarm, NULL);
+    sprintf(arg, "%d", sock);
+    pid = fork();
+    if (!pid) {
+      execl(selector, "selector", arg, NULL);
+    }
+    if (pid > 0) {
+      pid_lst *pids_new = malloc(sizeof(pid_lst));
+      if (!pids_first) {
+        pids_first = pids_new;
+        pids_new->prev = NULL;
+      } else {
+        pids_last->next = pids_new;
+        pids_new->prev = pids_last;
+      }
+      pids_last = pids_new;
+      pids_last->pid = pid;
+      pids_last->sock = sock;
+      pids_last->next = NULL;
+    }
+    sigprocmask(SIG_UNBLOCK, &block_alarm, NULL);
+  }
+  return 1;
+}
