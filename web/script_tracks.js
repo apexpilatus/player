970d3b4d276f16bbfe1627304672271f0f7b9cd7
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2024-02-11 16:26:31 +0100

    tiny

--- web/script_tracks.js
+++ web/script_tracks.js
@@ -1,8 +1,8 @@
 function hidetracks() {
     parent.document.getElementById("albums").style.height = "100%";
-    parent.document.getElementById("tracks").hidden = true;
+    parent.document.getElementById("tracks").style.display = "none";
 }
 
 function showtracks() {
-    parent.document.getElementById("tracks").hidden = false;
+    parent.document.getElementById("tracks").style.display = "inline";
 }
