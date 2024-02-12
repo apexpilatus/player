aa7fc1176ce2daedd6ab0ff8826a57a9cdd2b675
Max <apexpilatus@gmail.com>
2024-02-12 09:24:29 +0100

    start sorting

--- web/resp_tracks.c
+++ web/resp_tracks.c
@@ -15,6 +15,13 @@ typedef struct meta_list_t {
   char *rate;
 } meta_list;
 
+static inline void sort_tags(meta_list *list_first) {
+  for (meta_list *go_slow = list_first; go_slow && go_slow->next;
+       go_slow = go_slow->next)
+    for (meta_list *go_fast = go_slow->next; go_fast; go_fast = go_fast->next)
+      printf("go_slow - %s;go_fast - %s\n", go_slow->track, go_fast->track);
+}
+
 static inline void cpy_tags(meta_list *list, FLAC__StreamMetadata *tags) {
   for (int i = 0; i < tags->data.vorbis_comment.num_comments; i++) {
     if (!strncmp("ARTIST", (char *)tags->data.vorbis_comment.comments[i].entry,
@@ -95,6 +102,7 @@ static inline void list_tracks(char *msg) {
           cpy_tags(list_tmp, tags);
         }
     strcat(msg, "<table>");
+    sort_tags(list_first);
     list_tmp = list_first;
     while (list_tmp) {
       strcat(msg, "<tr>");
