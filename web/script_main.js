3ca69a532e725f20c01baa1c3126e0a75e71e895
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2024-02-17 21:13:16 +0100

    tiny

--- web/script_main.js
+++ web/script_main.js
@@ -7,6 +7,9 @@ function getvolume() {
         .then(resp => {
             if (resp.status == 200) {
                 document.getElementById("volume").innerHTML = "&#9738";
+                document.getElementById("control").min = resp.statusText.split("_")[0];
+                document.getElementById("control").value = resp.statusText.split("_")[1];
+                document.getElementById("control").max = resp.statusText.split("_")[2];
                 document.getElementById("control").hidden = false;
             } else {
                 document.getElementById("volume").innerHTML = "&#9739";
