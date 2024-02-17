9208df01c68229fb6b405dd69c0ec823f5ca6681
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2024-02-17 21:00:15 +0100

    tiny

--- web/script_main.js
+++ web/script_main.js
@@ -6,11 +6,7 @@ function getvolume() {
     fetch("getvolume")
         .then(resp => {
             if (resp.status == 200) {
-                text = resp.text.toString;
                 document.getElementById("volume").innerHTML = "&#9738";
-                document.getElementById("control").min = text.split(";")[0];
-                document.getElementById("control").value = text.split(";")[1];
-                document.getElementById("control").max = text.split(";")[2];
                 document.getElementById("control").hidden = false;
             } else {
                 document.getElementById("volume").innerHTML = "&#9739";
