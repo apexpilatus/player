3e85b920738d11cce064fdb019c526f0fd9cce32
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2024-02-18 09:23:53 +0100

    Delete meta directory

--- meta/shares.c
+++
@@ -1,9 +0,0 @@
-#include "shares.h"
-
-volatile FLAC__uint32 *length;
-int length_size = sizeof(FLAC__uint32);
-volatile FLAC__uint32 *length_half;
-int length_half_size = sizeof(FLAC__uint32);
-char *data_addr;
-char *data_addr_half;
-int data_size;
