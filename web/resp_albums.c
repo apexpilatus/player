43e960eaa77acef92e5642355bc7575a2929fb0e
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2024-05-24 22:58:37 +0200

    list albums through albums_list

--- web/resp_albums.c
+++ web/resp_albums.c
@@ -50,39 +50,14 @@ static inline albums_list *get_albums() {
 }
 
 static inline void list_albums(char *msg) {
-  char *src_path = malloc(getpagesize());
-  DIR *dp_music;
-  struct dirent *src_ep;
-  dp_music = opendir(music);
-  if (dp_music) {
-    while ((src_ep = readdir(dp_music)))
-      if (src_ep->d_type == DT_DIR && strcmp(src_ep->d_name, ".") &&
-          strcmp(src_ep->d_name, "..") &&
-          strcmp(src_ep->d_name, "lost+found")) {
-        DIR *dp_src;
-        struct dirent *albm_ep;
-        sprintf(src_path, "%s/%s", music, src_ep->d_name);
-        dp_src = opendir(src_path);
-        if (dp_src) {
-          while ((albm_ep = readdir(dp_src)))
-            if (albm_ep->d_type == DT_DIR && strcmp(albm_ep->d_name, ".") &&
-                strcmp(albm_ep->d_name, "..") &&
-                strcmp(albm_ep->d_name, "lost+found")) {
+albums_list *albums = get_albums();
+while(albums){
               strcat(msg, "<img src=\"");
-              strcat(msg, src_path);
-              strcat(msg, "/");
-              strcat(msg, albm_ep->d_name);
+              strcat(msg, albums->path);
               strcat(msg, "\" onclick=gettracks(\"");
-              strcat(msg, src_path);
-              strcat(msg, "/");
-              strcat(msg, albm_ep->d_name);
+              strcat(msg, albums->path);
               strcat(msg, "\") alt=picture>");
-            }
-          closedir(dp_src);
-        }
-      }
-    closedir(dp_music);
-  }
+              }
 }
 
 int main(int prm_n, char *prm[]) {
