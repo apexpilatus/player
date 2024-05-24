4675a5bcfd9b238f77e20d2b54f3d99d8331ab87
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2024-05-24 10:46:43 +0200

    fix

--- web/web.c
+++ web/web.c
@@ -87,7 +87,7 @@ static inline void selector(int sock) {
     }
     pid = fork();
     if (!pid)
-    if (system("/ending.sh")
+    if (system("/ending.sh"))
 execl(resp_err, "resp_err", sock_txt, NULL);
 else
       execl(system_poweroff, "system_poweroff", sock_txt, NULL);
