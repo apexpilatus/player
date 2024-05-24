c8d5edc294fcc8e78c2aaeaf8ea6caedf0dbcf38
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2024-05-24 22:40:28 +0200

    add albums_list

--- web/resp_albums.c
+++ web/resp_albums.c
@@ -4,6 +4,50 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct albums_list_t {
+  struct albums_list_t *next;
+  char *path;
+} albums_list;
+
+static inline albums_list get_albums(){
+  char *src_path = malloc(getpagesize());
+  albums_list *album_first = NULL, *album_tmp = NULL;
+  DIR *dp_music;
+  struct dirent *src_ep;
+  dp_music = opendir(music);
+  if (dp_music) {
+    while ((src_ep = readdir(dp_music)))
+      if (src_ep->d_type == DT_DIR && strcmp(src_ep->d_name, ".") &&
+          strcmp(src_ep->d_name, "..") &&
+          strcmp(src_ep->d_name, "lost+found")) {
+        DIR *dp_src;
+        struct dirent *albm_ep;
+        sprintf(src_path, "%s/%s", music, src_ep->d_name);
+        dp_src = opendir(src_path);
+        if (dp_src) {
+          while ((albm_ep = readdir(dp_src)))
+            if (albm_ep->d_type == DT_DIR && strcmp(albm_ep->d_name, ".") &&
+                strcmp(albm_ep->d_name, "..") &&
+                strcmp(albm_ep->d_name, "lost+found")) {
+                  if (!album_first) {
+                album_tmp = malloc(sizeof(albums_list));
+                album_first = album_tmp;
+              } else {
+                album_tmp->next = malloc(sizeof(albums_list));
+                album_tmp = album_tmp->next;
+              }
+              memset(album_tmp, 0, sizeof(albums_list));
+              album_tmp->path=malloc(strlen(src_path) + strlen(albm_ep->d_name) + 2);
+              sprintf(album_tmp->path, "%s/%s", src_path, albm_ep->d_name);
+            }
+          closedir(dp_src);
+        }
+      }
+    closedir(dp_music);
+  }
+  return album_first;
+}
+
 static inline void list_albums(char *msg) {
   char *src_path = malloc(getpagesize());
   DIR *dp_music;
