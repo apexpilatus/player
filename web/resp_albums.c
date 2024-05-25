003ff2bd4764a5f80986c63f7afc4e0319faa5a3
apexpilatus <35628650+apexpilatus@users.noreply.github.com>
2024-05-25 11:16:28 +0200

    atime

--- web/resp_albums.c
+++ web/resp_albums.c
@@ -8,7 +8,7 @@
 typedef struct albums_list_t {
   struct albums_list_t *next;
   char *path;
-  unsigned long int st_atime_nsec;
+  time_t st_atime;
 } albums_list;
 
 static inline albums_list *get_albums() {
@@ -44,9 +44,7 @@ static inline albums_list *get_albums() {
                   malloc(strlen(src_path) + strlen(albm_ep->d_name) + 2);
               sprintf(album_tmp->path, "%s/%s", src_path, albm_ep->d_name);
               if (stat(album_tmp->path, &stat_buf))
-              album_tmp->st_atime_nsec = stat_buf.st_atime_nsec;
-            else
-            album_tmp->st_atime_nsec = 0;
+              album_tmp->st_atime = stat_buf.st_atime;
             }
           closedir(dp_src);
         }
