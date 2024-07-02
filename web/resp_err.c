cd1802e38c56758328eda719010dfd57bae04092
Max <apexpilatus@gmail.com>
2024-07-02 16:27:52 +0200

    tiny

--- web/resp_err.c
+++ web/resp_err.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -6,8 +7,8 @@ int main(int prm_n, char *prm[]) {
   int sock = strtol(prm[1], NULL, 10);
   ssize_t rsp_size = getpagesize(), write_size;
   char *rsp = malloc(rsp_size);
-  strcpy(rsp, "HTTP/1.1 404 shit happens\r\nCache-control: "
-              "no-cache\r\nX-Content-Type-Options: nosniff\r\n\r\n");
+  sprintf(rsp, "%s\r\n%s\r\n%s\r\n\r\n", "HTTP/1.1 404 shit happens",
+          "Cache-control: no-cache", "X-Content-Type-Options: nosniff");
   write_size = write(sock, rsp, strlen(rsp));
   if (write_size == strlen(rsp))
     return 0;
