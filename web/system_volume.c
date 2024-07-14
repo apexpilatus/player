fd8ec22d8325f28047ee682e7ace63b3684204bf
Max <apexpilatus@gmail.com>
2024-07-14 12:37:29 +0200

    refactor system_volume

--- web/system_volume.c
+++ web/system_volume.c
@@ -13,39 +13,54 @@
 #include <alsa/mixer.h>
 // clang-format on
 
-int main(int prm_n, char *prm[]) {
-  int card_number = -1, sock = strtol(prm[1], NULL, 10);
-  ssize_t rsp_size = getpagesize(), write_size;
-  char card_name[10], *rsp = malloc(rsp_size), *url = prm[2], *url_vol;
-  long min_vol, max_vol, curr_vol;
+static inline int init_mixer(snd_mixer_elem_t **melem, long *min_vol,
+                             long *max_vol, long *curr_vol) {
+  int card_number = -1;
+  char card_name[10];
   snd_mixer_t *mxr;
-  snd_mixer_elem_t *melem;
   if (snd_card_next(&card_number) || card_number == -1 ||
       snd_mixer_open(&mxr, 0))
-    execl(resp_err, "resp_err", prm[1], NULL);
+    return 1;
   sprintf(card_name, "hw:%d", card_number);
   if (snd_mixer_attach(mxr, card_name) ||
       snd_mixer_selem_register(mxr, NULL, NULL) || snd_mixer_load(mxr) ||
-      !(melem = snd_mixer_first_elem(mxr)))
-    execl(resp_err, "resp_err", prm[1], NULL);
-  for (; melem && !snd_mixer_selem_has_playback_volume_joined(melem);
-       melem = snd_mixer_elem_next(melem))
+      !(*melem = snd_mixer_first_elem(mxr)))
+    return 1;
+  for (; *melem && !snd_mixer_selem_has_playback_volume_joined(*melem);
+       *melem = snd_mixer_elem_next(*melem))
     ;
-  if (!melem)
+  if (!(*melem))
+    return 1;
+  if (snd_mixer_selem_get_playback_volume_range(*melem, min_vol, max_vol) ||
+      snd_mixer_selem_get_playback_volume(*melem, SND_MIXER_SCHN_UNKNOWN,
+                                          curr_vol))
+    return 1;
+  return 0;
+}
+
+int main(int prm_n, char *prm[]) {
+  int sock = strtol(prm[1], NULL, 10);
+  ssize_t rsp_size = getpagesize();
+  ssize_t write_size;
+  char *rsp = malloc(rsp_size);
+  char *url = prm[2];
+  char *url_vol;
+  long min_vol;
+  long max_vol;
+  long curr_vol;
+  snd_mixer_elem_t *melem;
+  if (init_mixer(&melem, &min_vol, &max_vol, &curr_vol))
     execl(resp_err, "resp_err", prm[1], NULL);
-  snd_mixer_selem_get_playback_volume_range(melem, &min_vol, &max_vol);
-  if (!(strcmp("/getvolume", url))) {
-    snd_mixer_selem_get_playback_volume(melem, SND_MIXER_SCHN_UNKNOWN,
-                                        &curr_vol);
+  if (!(strcmp("/getvolume", url)))
     sprintf(rsp, "HTTP/1.1 200 %ld_%ld_%ld\r\n", min_vol, curr_vol, max_vol);
-  } else {
+  else {
     url_vol = strchr(url, '&');
-    if (!(url_vol && (curr_vol = strtol(++url_vol, NULL, 10)) &&
-          curr_vol <= max_vol && curr_vol >= min_vol))
+    if (!(url_vol && (curr_vol = strtol(++url_vol, NULL, 10)) <= max_vol &&
+          curr_vol >= min_vol) ||
+        snd_mixer_selem_set_playback_volume(melem, SND_MIXER_SCHN_UNKNOWN,
+                                            curr_vol))
       execl(resp_err, "resp_err", prm[1], NULL);
     strcpy(rsp, "HTTP/1.1 200 OK\r\n");
-    snd_mixer_selem_set_playback_volume(melem, SND_MIXER_SCHN_UNKNOWN,
-                                        curr_vol);
   }
   strcat(rsp, "Content-Type: text/html; charset=utf-8\r\n");
   strcat(rsp, "Cache-control: no-cache\r\n");
