2550a231dbc08fc3018595464aa4b468b5400ed5
Max Otrishko <35628650+apexpilatus@users.noreply.github.com>
2024-07-21 08:55:11 +0000

    format

--- web/script_albums.js
+++ web/script_albums.js
@@ -3,11 +3,11 @@ let current = "empty";
 function gettracks(album) {
     if (current == album) {
         fetch("play?" + album)
-        .then(resp => {
-            if (resp.status == 200) {
-                location.reload();
-            }
-        })
+            .then(resp => {
+                if (resp.status == 200) {
+                    location.reload();
+                }
+            })
     } else {
         current = album.slice();
         parent.document.getElementById("tracks").hidden = true;
