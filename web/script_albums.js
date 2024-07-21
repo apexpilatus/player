c6d3cf9aabcef89bfbe3d1f67739d111102001b4
Max <apexpilatus@gmail.com>
2024-07-21 10:50:05 +0200

    update albums after playing

--- web/script_albums.js
+++ web/script_albums.js
@@ -2,7 +2,12 @@ let current = "empty";
 
 function gettracks(album) {
     if (current == album) {
-        fetch("play?" + album);
+        fetch("play?" + album)
+        .then(resp => {
+            if (resp.status == 200) {
+                location.reload();
+            }
+        })
     } else {
         current = album.slice();
         parent.document.getElementById("tracks").hidden = true;
