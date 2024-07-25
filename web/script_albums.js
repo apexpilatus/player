1b9f230c9090a70aa421d0b8a7f614afa789b731
Max <apexpilatus@gmail.com>
2024-07-25 17:07:55 +0200

    add checkvolume to tracks

--- web/script_albums.js
+++ web/script_albums.js
@@ -3,19 +3,20 @@ let current = "empty";
 function gettracks(album) {
     if (current == album) {
         fetch("getvolume")
-        .then(resp => {
-            if (resp.status == 200) {
-            if (resp.statusText.split("_")[1] == resp.statusText.split("_")[2]) {
-                fetch("setvolume&" + resp.statusText.split("_")[0])
-            }
-            }
-        })
-        fetch("play?" + album)
             .then(resp => {
                 if (resp.status == 200) {
-                    location.reload();
+                    if (resp.statusText.split("_")[1] == resp.statusText.split("_")[2]) {
+                        fetch("setvolume&" + resp.statusText.split("_")[0])
+                    }
+                    fetch("play?" + album)
+                        .then(resp => {
+                            if (resp.status == 200) {
+                                location.reload();
+                            }
+                        })
                 }
             })
+
     } else {
         current = album.slice();
         parent.document.getElementById("tracks").hidden = true;
--- web/script_tracks.js
+++ web/script_tracks.js
@@ -3,7 +3,13 @@ function showtracks() {
 }
 
 function play(dirtrack) {
-    let elem = document.getElementById("rate");
-    elem.style.color = "gray";
-    fetch("play?" + dirtrack);
+    fetch("getvolume")
+        .then(resp => {
+            if (resp.status == 200) {
+                if (resp.statusText.split("_")[1] == resp.statusText.split("_")[2]) {
+                    fetch("setvolume&" + resp.statusText.split("_")[0])
+                }
+                fetch("play?" + dirtrack);
+            }
+        })
 }
