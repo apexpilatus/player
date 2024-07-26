c091b9eca36482905dfea22b614b0059c8cd8f6c
Max <apexpilatus@gmail.com>
2024-07-26 11:28:09 +0200

    update volume on screen

--- web/script_albums.js
+++ web/script_albums.js
@@ -5,7 +5,10 @@ function gettracks(album) {
         fetch("getvolume").then(resp => {
             if (resp.status == 200) {
                 if (resp.statusText.split("_")[1] == resp.statusText.split("_")[2]) {
-                    fetch("setvolume&" + resp.statusText.split("_")[0])
+                    fetch("setvolume&" + resp.statusText.split("_")[0]);
+                    if (!parent.document.getElementById("control").hidden) {
+                        parent.document.getElementById("control").value = resp.statusText.split("_")[0];
+                    }
                 }
             }
         })
