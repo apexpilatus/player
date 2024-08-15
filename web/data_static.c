54c43452e069a85214982841a4b1fc0180b3580a
Max <apexpilatus@gmail.com>
2024-08-15 22:56:52 +0200

    play with favicons

--- web/data_static.c
+++ web/data_static.c
@@ -10,8 +10,9 @@ extern unsigned char style_tracks_css[];
 extern unsigned char script_tracks_js[];
 extern unsigned char style_albums_css[];
 extern unsigned char script_albums_js[];
-extern unsigned char favicon32_png[];
-extern unsigned char favicon192_png[];
+extern unsigned char favicon_ico[];
+extern unsigned char favicon152_png[];
+extern unsigned char favicon180_png[];
 extern unsigned int index_html_len;
 extern unsigned int style_main_css_len;
 extern unsigned int script_main_js_len;
@@ -19,29 +20,37 @@ extern unsigned int style_tracks_css_len;
 extern unsigned int script_tracks_js_len;
 extern unsigned int style_albums_css_len;
 extern unsigned int script_albums_js_len;
-extern unsigned int favicon32_png_len;
-extern unsigned int favicon192_png_len;
+extern unsigned int favicon_ico_len;
+extern unsigned int favicon152_png_len;
+extern unsigned int favicon180_png_len;
 
 unsigned char *data;
 unsigned int data_len;
 
 static inline int select_data(char *url, char *rsp) {
   unsigned long hdr_end;
+  printf("%s\n", url);
   if (!strcmp("/", url)) {
     data = index_html;
     data_len = index_html_len;
     sprintf(rsp, "%s\r\n%s\r\n%s\r\n", "HTTP/1.1 200 OK",
             "Content-Type: text/html; charset=utf-8",
             "Cache-control: no-cache");
-  } else if (!strcmp("/favicon32.png", url)) {
-    data = favicon32_png;
-    data_len = favicon32_png_len;
+  } else if (!strcmp("/favicon.ico", url)) {
+    data = favicon_ico;
+    data_len = favicon_ico_len;
+    sprintf(rsp, "%s\r\n%s\r\n%s\r\n", "HTTP/1.1 200 OK",
+            "Content-Type: image/x-icon",
+            "Cache-control: max-age=31536000, immutable");
+  } else if (!strcmp("/apple-touch-icon-precomposed.png", url)) {
+    data = favicon152_png;
+    data_len = favicon152_png_len;
     sprintf(rsp, "%s\r\n%s\r\n%s\r\n", "HTTP/1.1 200 OK",
             "Content-Type: image/png",
             "Cache-control: max-age=31536000, immutable");
-  } else if (!strcmp("/favicon192.png", url)) {
-    data = favicon192_png;
-    data_len = favicon192_png_len;
+  } else if (!strcmp("/apple-touch-icon.png", url)) {
+    data = favicon180_png;
+    data_len = favicon180_png_len;
     sprintf(rsp, "%s\r\n%s\r\n%s\r\n", "HTTP/1.1 200 OK",
             "Content-Type: image/png",
             "Cache-control: max-age=31536000, immutable");
