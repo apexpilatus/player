94282c7f046d357f8b8f4e0b196b3defb731108b
Max <apexpilatus@gmail.com>
2025-01-03 19:27:24 +0100

    add foreground service

--- streamer/app/src/main/java/home/music/streamer/MainActivity.kt
+++ streamer/app/src/main/java/home/music/streamer/MainActivity.kt
@@ -1,31 +1,13 @@
 package home.music.streamer
 
 import android.app.Activity
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
+import android.content.Intent
 import android.os.Bundle
 
-const val CHANNEL_ID = "main"
-const val CHANNEL_NAME = "main"
-
-class MainActivity: Activity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(
-            NotificationChannel(
-                CHANNEL_ID,
-                CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-        )
-        notificationManager.notify(1, Notification.Builder(applicationContext, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_stat_name)
-            .setContentText("trtr")
-            .build())
-
+        startForegroundService(Intent(this, MainService::class.java))
         finishAndRemoveTask()
     }
 }
---
+++ streamer/app/src/main/java/home/music/streamer/MainService.kt
@@ -0,0 +1,47 @@
+package home.music.streamer
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+const val CHANNEL_ID = "main"
+const val CHANNEL_NAME = "main"
+
+class MainService : Service() {
+    private val notificationManager by lazy { getSystemService(NOTIFICATION_SERVICE) as NotificationManager }
+    override fun onCreate() {
+        notificationManager.createNotificationChannel(
+            NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+        )
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        this.startForeground(
+            1, Notification.Builder(applicationContext, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentText("trtr")
+                .build()
+        )
+        Thread.sleep(10000)
+        notificationManager.notify(
+            1, Notification.Builder(applicationContext, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentText("brbr")
+                .build()
+        )
+        Thread.sleep(10000)
+        stopSelf()
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
