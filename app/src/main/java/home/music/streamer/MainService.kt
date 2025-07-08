b7a5b1c414936c5d3fc6e4dffc818ee89c3b5ebf
Max <apexpilatus@gmail.com>
2025-07-08 20:43:56 +0200

    go back to MediaPlayer

--- app/src/main/java/home/music/streamer/MainService.kt
+++ app/src/main/java/home/music/streamer/MainService.kt
@@ -6,114 +6,59 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.IBinder
 import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStream
 import java.io.InputStreamReader
-import java.io.OutputStreamWriter
 import java.net.ServerSocket
-import java.net.Socket
 import java.net.URL
-import java.util.Arrays
 
 const val CHANNEL_ID = "main"
 const val CHANNEL_NAME = "main"
 
-class MainService : Service() {
+class MainService : Service(), MediaPlayer.OnCompletionListener {
     private val sockServer by lazy { ServerSocket(9696) }
     private val notificationManager by lazy { getSystemService(NOTIFICATION_SERVICE) as NotificationManager }
+    private val player by lazy {
+        MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA).build()
+            )
+            setOnCompletionListener(this@MainService)
+        }
+    }
 
     companion object {
         @Volatile
         var started = false
-
-        @Volatile
-        var stopPlaying = false
-    }
-
-    private fun checkHeader(reader: InputStream): Boolean {
-        val hdr: MutableList<Byte> = mutableListOf()
-        hdr.addAll(reader.readNBytes(1).asList())
-        if (hdr.isNotEmpty()) {
-            var hdrSize = hdr.size
-            val maxSize = 1000
-            while (hdrSize < maxSize) {
-                hdr.addAll(reader.readNBytes(1).asList())
-                if (hdr.size == hdrSize) {
-                    return false
-                }
-                if (hdr.size > 3 && Arrays.compare(
-                        hdr.toByteArray(),
-                        hdr.size - 4,
-                        hdr.size - 1,
-                        "\r\n\r\n".toByteArray(),
-                        0,
-                        3
-                    ) == 0
-                ) break
-                hdrSize = hdr.size
-            }
-            if (hdrSize == maxSize) return false
-            if (String(hdr.toByteArray()).contains("HTTP/1.1 200 OK")) return true
-        }
-        return false
     }
 
     private val listen = Thread {
         var remoteIP: String
         var url: String
-        var play = Thread {
-            notificationManager.notify(
-                1,
-                Notification.Builder(this, CHANNEL_ID).setSmallIcon(R.drawable.ic_notification)
-                    .setShowWhen(false).setOnlyAlertOnce(true).setContentText("waiting").build()
-            )
-        }
-        play.start()
         while (true) {
             try {
                 sockServer.accept().use {
-                    stopPlaying = true
-                    play.join()
-                    stopPlaying = false
                     remoteIP = it.remoteSocketAddress.toString().replace("/", "").split(":")[0]
                     url = BufferedReader(InputStreamReader(it.getInputStream())).readLine()
-                    play = Thread {
-                        try {
-                            Socket(remoteIP, 80).use { socket ->
-                                val writer =
-                                    BufferedWriter(OutputStreamWriter(socket.getOutputStream()))
-                                writer.write("GET $url \r\n\r\n")
-                                writer.flush()
-                                val reader = socket.getInputStream()
-                                if (checkHeader(reader)) {
-                                    URL(
-                                        "http://$remoteIP/apple-touch-icon.png"
-                                    ).openStream().use { streamPicture ->
-                                        notificationManager.notify(
-                                            1,
-                                            Notification.Builder(this, CHANNEL_ID)
-                                                .setSmallIcon(R.drawable.ic_notification)
-                                                .setLargeIcon(BitmapFactory.decodeStream(streamPicture))
-                                                .setOnlyAlertOnce(true).setShowWhen(false)
-                                                .setContentText("").build()
-                                        )
-                                    }
-                                    Streamer(reader).play()
-                                    notificationManager.notify(
-                                        1,
-                                        Notification.Builder(this, CHANNEL_ID)
-                                            .setSmallIcon(R.drawable.ic_notification)
-                                            .setOnlyAlertOnce(true).setShowWhen(false)
-                                            .setContentText("").build()
-                                    )
-                                }
-                            }
-                        } catch (_: Exception) {
-                        }
+                    with(player) {
+                        reset()
+                        setDataSource("http://$remoteIP$url")
+                        prepare()
+                        start()
                     }
-                    play.start()
+                    URL("http://$remoteIP/apple-touch-icon.png").openStream().use { picture ->
+                        notificationManager.notify(
+                            1,
+                            Notification.Builder(this, CHANNEL_ID)
+                                .setSmallIcon(R.drawable.ic_notification)
+                                .setLargeIcon(BitmapFactory.decodeStream(picture))
+                                .setOnlyAlertOnce(true).setShowWhen(false).setContentText("")
+                                .build()
+                        )
+                    }
                 }
             } catch (_: Exception) {
             }
@@ -146,5 +91,13 @@ class MainService : Service() {
     override fun onDestroy() {
         sockServer.close()
         started = false
+    }
+
+    override fun onCompletion(mp: MediaPlayer?) {
+        notificationManager.notify(
+            1,
+            Notification.Builder(this, CHANNEL_ID).setSmallIcon(R.drawable.ic_notification)
+                .setOnlyAlertOnce(true).setShowWhen(false).setContentText("").build()
+        )
     }
 }
