687ef0bbc2fc7cf7cd793c0fd36a1e9e383b5dba
Max Otrishko <35628650+apexpilatus@users.noreply.github.com>
2025-11-13 21:40:17 +0100

    set class

--- web/static/script_tracks.js
+++ web/static/script_tracks.js
@@ -5,6 +5,7 @@ const topalbumElem = parent.document.getElementById("topalbum");
 const playerElem = parent.document.getElementById("player");
 
 function showtracks() {
+    controlElem.className = "cdcontrol";
     controlElem.hidden = false;
 }
 
