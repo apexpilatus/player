2676386e24b68a0c630a21aa0777fd23f51bc650
Max Otrishko <35628650+apexpilatus@users.noreply.github.com>
2025-11-20 07:26:39 +0100

    fix

--- web/static/script_albums.js
+++ web/static/script_albums.js
@@ -1,7 +1,7 @@
 const iconElem = parent.document.getElementById("icon");
 
 function gettracks(album) {
-    alert(iconElem.href + " - " + location.origin + "/" + album);
+    alert(iconElem.href + " - " + location.origin + album);
     if (iconElem.href != album)
         parent.location.assign(location.origin + "/tracks?" + "album=" + album + "&scroll=" + pageYOffset);
 }
