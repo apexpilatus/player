93a0c1430b14c3561a9b618a068a409ee32347c1
Max <apexpilatus@gmail.com>
2025-12-28 16:49:12 +0100

    check parent

--- web/static/script_albums.js
+++ web/static/script_albums.js
@@ -1,14 +1,12 @@
+const iconElem = parent.document.getElementById("icon");
+const topalbumElem = parent.document.getElementById("topalbum");
+
 function gettracks(album) {
-        if (window.parent) {
-                const iconElem = parent.document.getElementById("icon");
-                if (iconElem.href != location.origin + album)
-                        parent.location.assign(location.origin + "/tracks?" + "album=" + album + "&scroll=" + pageYOffset);
-        }
+	if(iconElem != null && iconElem.href != location.origin + album)
+                parent.location.assign(location.origin + "/tracks?" + "album=" + album + "&scroll=" + pageYOffset);
 }
 
 function updatetop(album) {
-        if (window.parent) {
-                const topalbumElem = parent.document.getElementById("topalbum");
-                topalbumElem.innerHTML = album;
-        }
+	if(topalbumElem != null)
+        topalbumElem.innerHTML = album;
 }
