f1c497213ab64d88a59ef26498166e7d35ef997c
Max Otrishko <35628650+apexpilatus@users.noreply.github.com>
2026-01-21 17:54:38 +0000

    remove play from albums script

--- web/static/script_albums.js
+++ web/static/script_albums.js
@@ -2,19 +2,12 @@ const iconElem = parent.document.getElementById("icon");
 const topalbumElem = parent.document.getElementById("topalbum");
 
 function gettracks(album) {
-        if (iconElem != null) {
-                if (iconElem.href != location.origin + album)
-                        parent.location.assign(location.origin + "/tracks?" + "album=" + album + "&scroll=" + pageYOffset);
-        } else
-                fetch("http://store/playflac?" + album).then(resp => {
-                        if (resp.status == 200)
-                                location.assign(location.origin + "/albums?scroll=0");
-                });
+        if (iconElem.href != location.origin + album)
+                parent.location.assign(location.origin + "/tracks?" + "album=" + album + "&scroll=" + pageYOffset);
 }
 
 function updatetop(album) {
-        if (topalbumElem != null)
-                topalbumElem.innerHTML = album;
+        topalbumElem.innerHTML = album;
 }
 
 function loaded(scroll) {
