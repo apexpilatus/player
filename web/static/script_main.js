be1c39775726524b036aebf6306eebb475d35a70
Max Otrishko <35628650+apexpilatus@users.noreply.github.com>
2026-01-21 16:27:25 +0000

    update play functions

--- web/static/script_main.js
+++ web/static/script_main.js
@@ -8,18 +8,15 @@ function getcd() {
 }
 
 function playflac(dirtrack) {
-    fetch(location.origin + "/playflac?" + dirtrack).then(resp => {
+    fetch(location.origin + "/touch?" + dirtrack.split("&")[0]).then(resp => {
         if (resp.status == 200) {
             playerElem.src = location.origin + "/stream_album?" + dirtrack;
-        if (topalbumElem.innerHTML != dirtrack.split("&")[0])
-            albumsElem.src = location.origin + "/albums?scroll=0";
+            if (topalbumElem.innerHTML != dirtrack.split("&")[0])
+                albumsElem.src = location.origin + "/albums?scroll=0";
         }
     });
 }
 
 function playcd(track) {
-    fetch(location.origin + "/playcd?" + track).then(resp => {
-        if (resp.status == 200)
-            playerElem.src = location.origin + "/stream_cd?" + track;
-    });
+    playerElem.src = location.origin + "/stream_cd?" + track;
 }
