a850d4ae2bb414ada41f258f362a9e19dfc8d51f
Max <apexpilatus@gmail.com>
2026-04-30 08:04:55 +0200

    refactor

--- web/src/resp_static.rs
+++ web/src/resp_static.rs
@@ -11,48 +11,55 @@ struct StaticData {
     bytes: Vec<u8>,
 }
 
-pub fn send_static(url: &str, mut stream: TcpStream) {
-    let mut data_static = HashMap::new();
-    data_static.insert(
-        "/favicon.ico",
+fn data_map() -> HashMap<String, StaticData> {
+    let mut data = HashMap::new();
+    data.insert(
+        "/favicon.ico".to_string(),
         StaticData {
             content_type: "image/x-icon".to_string(),
             bytes: include_bytes!("../static/favicon.ico").to_vec(),
         },
     );
-    data_static.insert(
-        "/apple-touch-icon.png",
+    data.insert(
+        "/apple-touch-icon.png".to_string(),
         StaticData {
             content_type: "image/png".to_string(),
             bytes: include_bytes!("../static/favicon180.png").to_vec(),
         },
     );
-    data_static.insert(
-        "/style_main.css",
+    data.insert(
+        "/style_main.css".to_string(),
         StaticData {
             content_type: "text/css; charset=utf-8".to_string(),
             bytes: include_bytes!("../static/style_main.css").to_vec(),
         },
     );
-    if let Some(resourse) = data_static.get(url) {
-        let hdr = format!(
-            "\
+    data
+}
+
+pub fn send_static(url: &str, mut stream: TcpStream) {
+    let static_data = data_map();
+    match static_data.get(url) {
+        Some(resourse) => {
+            let hdr = format!(
+                "\
 HTTP/1.1 200 OK\r\n\
 X-Content-Type-Options: nosniff\r\n\
 Cache-control: max-age=31536000, immutable\r\n\
 Content-Type: {}\r\n\
 Content-Length: {}\r\n\r\n",
-            resourse.content_type,
-            resourse.bytes.len()
-        );
-        match stream.write_all(hdr.as_bytes()) {
-            Ok(_) => match stream.write_all(&resourse.bytes) {
-                _ => return,
-            },
-            Err(_) => return,
+                resourse.content_type,
+                resourse.bytes.len()
+            );
+            match stream.write_all(hdr.as_bytes()) {
+                Ok(_) => match stream.write_all(&resourse.bytes) {
+                    _ => (),
+                },
+                Err(_) => (),
+            }
         }
-    }
-    match stream.write_all(ERR.as_bytes()) {
-        _ => (),
+        None => match stream.write_all(ERR.as_bytes()) {
+            _ => (),
+        },
     }
 }
