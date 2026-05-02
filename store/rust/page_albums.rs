0fbbd3539413164f2a1e28f5fdbe7d013f4aaabd
Max <apexpilatus@gmail.com>
2026-05-02 15:44:50 +0000

    get_albums()

--- store/rust/page_albums.rs
+++ store/rust/page_albums.rs
@@ -1,8 +1,36 @@
+use std::fs;
 use std::io::Write;
+use std::time::SystemTime;
 use TcpStream;
 
+struct Album {
+    name: String,
+    mtime: SystemTime,
+}
+
 pub fn send_albums(mut stream: TcpStream) {
-    let html = include_str!("../html/albums.html");
+    let mut html = String::from(include_str!("../html/albums.html"));
+    if let Some(into) = html.find("</body>") {
+        if let Ok(dir) = fs::read_dir(env!("MUSIC_PATH")) {
+            let mut albums: Vec<Album> = Vec::new();
+            for album in dir {
+                if let Ok(album) = album {
+                    if let Ok(name) = album.file_name().into_string() {
+                        if let Ok(meta) = album.metadata() {
+                            if let Ok(mtime) = meta.modified() {
+                                albums.push(Album { name, mtime });
+                            }
+                        }
+                    }
+                }
+            }
+            albums.sort_by(|alb1, alb2| alb1.mtime.cmp(&alb2.mtime));
+            for album in albums {
+                let album = format!("<img src=\"picture?album={}\" alt=\"picture\">", album.name);
+                html.insert_str(into, &album);
+            }
+        }
+    }
     let hdr = format!(
         "\
 HTTP/1.1 200 OK\r\n\
