4ed4692749f947b1fa3e27f944fa083338b07b7e
Max <apexpilatus@gmail.com>
2026-05-07 13:36:16 +0200

    404 on stream

--- store/rust/data_extracted.rs
+++ store/rust/data_extracted.rs
@@ -1,3 +1,10 @@
+use err_codes;
+use std::io::Write;
 use TcpStream;
 
-pub fn send_extracted(params: Option<&str>, range: &str, stream: TcpStream) {}
+pub fn send_extracted(params: Option<&str>, range: &str, mut stream: TcpStream) {
+    println!("{range}");
+    match stream.write_all(err_codes::ERR_404.as_bytes()) {
+        _ => (),
+    }
+}
