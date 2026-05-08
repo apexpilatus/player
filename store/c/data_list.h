4af7f8548d093ed2a2c5e36d34c0e4aeaede06e1
Max <apexpilatus@gmail.com>
2026-05-08 13:29:12 +0200

    add flac extract

---
+++ store/c/data_list.h
@@ -0,0 +1,9 @@
+typedef struct data_list_t {
+  struct data_list_t volatile *next;
+  char volatile *buf;
+  int data_size;
+} data_list;
+
+extern const unsigned int data_buf_size;
+
+extern int buf_len(data_list volatile *data);
