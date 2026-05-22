8bbdd29c3e673d04a99d05dfe7325ec0d59fda83
Max Otrishko <35628650+apexpilatus@users.noreply.github.com>
2026-05-22 15:23:37 +0200

    draft for web

---
+++ streamer/rust/data_static.rs
@@ -0,0 +1,49 @@
+use err_codes;
+use std::collections::HashMap;
+use std::io::Write;
+use BufWriter;
+use TcpStream;
+
+struct StaticData {
+    content_type: String,
+    bytes: Vec<u8>,
+}
+
+fn data_map() -> HashMap<String, StaticData> {
+    let mut data = HashMap::new();
+    data.insert(
+        "/favicon.ico".to_string(),
+        StaticData {
+            content_type: "image/x-icon".to_string(),
+            bytes: include_bytes!("../pictures/favicon.ico").to_vec(),
+        },
+    );
+    data
+}
+
+pub fn send_static(url: &str, mut stream: BufWriter<TcpStream>) {
+    let static_data = data_map();
+    match static_data.get(url) {
+        Some(resourse) => {
+            let hdr = format!(
+                "\
+HTTP/1.1 200 OK\r\n\
+X-Content-Type-Options: nosniff\r\n\
+Cache-control: max-age=31536000, immutable\r\n\
+Content-Type: {}\r\n\
+Content-Length: {}\r\n\r\n",
+                resourse.content_type,
+                resourse.bytes.len()
+            );
+            match stream.write_all(hdr.as_bytes()) {
+                Ok(_) => match stream.write_all(&resourse.bytes) {
+                    _ => (),
+                },
+                Err(_) => return,
+            }
+        }
+        None => match stream.write_all(err_codes::ERR_404.as_bytes()) {
+            _ => (),
+        },
+    }
+}
---
+++ streamer/rust/web.rs
@@ -0,0 +1,44 @@
+mod data_static;
+use std::io::{BufRead, BufReader, BufWriter};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn selector(stream: TcpStream) {
+    let reader = BufReader::new(&stream);
+    let mut req: Vec<String> = Vec::new();
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                if line.is_empty() {
+                    break;
+                }
+                req.push(line);
+            }
+            Err(_) => return,
+        }
+    }
+    if !req.is_empty() {
+        if let Some(url) = req[0].split(" ").nth(1) {
+            let mut url = url.split("?");
+            if let Some(path) = url.next() {
+                let params = url.next();
+                match path {
+                    _ => data_static::send_static(path, BufWriter::new(stream)),
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let bind_addr = env!("BIND_ADDR");
+    let listener = match TcpListener::bind(bind_addr) {
+        Ok(listener) => listener,
+        Err(_) => return,
+    };
+    for stream in listener.incoming() {
+        if let Ok(stream) = stream {
+            thread::spawn(move || selector(stream));
+        };
+    }
+}
