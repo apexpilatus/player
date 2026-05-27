4ef5c389fdc63bc009d6284ce475454de90d99de
Max <apexpilatus@gmail.com>
2026-05-27 11:40:03 +0200

    format

--- streamer/rust/proxy.rs
+++ streamer/rust/proxy.rs
@@ -1,28 +1,30 @@
 use err_codes;
-use std::io::Write;
+use std::io::{Read, Write};
 use BufWriter;
 use TcpStream;
 
 pub fn forward(mut streamer: BufWriter<TcpStream>) {
     match TcpStream::connect(env!("STORE_ADDR")) {
-              Ok(store) =>  {let mut buf: Vec<u8> = vec![0; streamer.capacity()];
-                loop {
-                    match store.read(&mut buf) {
-                        Ok(size) => {
-                            if size == 0 {
-                                break;
-                            }
-                            match streamer.write_all(&buf[..size]) {
-                                Ok(_) => (),
-                                Err(_) => break,
-                            }
+        Ok(mut store) => {
+            let mut buf: Vec<u8> = vec![0; streamer.capacity()];
+            loop {
+                match store.read(&mut buf) {
+                    Ok(size) => {
+                        if size == 0 {
+                            break;
                         }
-                        Err(_) => break,
+                        match streamer.write_all(&buf[..size]) {
+                            Ok(_) => (),
+                            Err(_) => break,
+                        }
                     }
+                    Err(_) => break,
                 }
-        return;},
-    Err(_) => match streamer.write_all(err_codes::ERR_404.as_bytes()) {
-        _ => (),
-    }
+            }
+            return;
+        }
+        Err(_) => match streamer.write_all(err_codes::ERR_404.as_bytes()) {
+            _ => (),
+        },
     }
 }
