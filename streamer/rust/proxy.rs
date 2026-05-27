9fedd04bca85f7ac775dd238a8aafff8e4dd3aeb
Max Otrishko <35628650+apexpilatus@users.noreply.github.com>
2026-05-27 11:34:39 +0200

    try to forward data from store

--- streamer/rust/proxy.rs
+++ streamer/rust/proxy.rs
@@ -4,11 +4,25 @@ use BufWriter;
 use TcpStream;
 
 pub fn forward(mut streamer: BufWriter<TcpStream>) {
-    if let Ok(store) = TcpStream::connect(env!("STORE_ADDR")) {
-        println!("ok");
-        return;
-    }
-    match streamer.write_all(err_codes::ERR_404.as_bytes()) {
+    match TcpStream::connect(env!("STORE_ADDR")) {
+              Ok(store) =>  {let mut buf: Vec<u8> = vec![0; streamer.capacity()];
+                loop {
+                    match store.read(&mut buf) {
+                        Ok(size) => {
+                            if size == 0 {
+                                break;
+                            }
+                            match streamer.write_all(&buf[..size]) {
+                                Ok(_) => (),
+                                Err(_) => break,
+                            }
+                        }
+                        Err(_) => break,
+                    }
+                }
+        return;},
+    Err(_) => match streamer.write_all(err_codes::ERR_404.as_bytes()) {
         _ => (),
+    }
     }
 }
