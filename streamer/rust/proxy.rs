3f52a4f042c3211c37c4988e69b563ec9969217c
Max <apexpilatus@gmail.com>
2026-05-27 11:29:17 +0200

    format

--- streamer/rust/proxy.rs
+++ streamer/rust/proxy.rs
@@ -4,11 +4,11 @@ use BufWriter;
 use TcpStream;
 
 pub fn forward(mut streamer: BufWriter<TcpStream>) {
-    if let Ok(store) = TcpStream::connect(env!("STORE_ADDR")){
+    if let Ok(store) = TcpStream::connect(env!("STORE_ADDR")) {
         println!("ok");
         return;
     }
-    match stream.write_all(err_codes::ERR_404.as_bytes()) {
+    match streamer.write_all(err_codes::ERR_404.as_bytes()) {
         _ => (),
     }
 }
