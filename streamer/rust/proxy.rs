6e88b0562002412a350e414a4c509de03ec25f85
Max <apexpilatus@gmail.com>
2026-05-27 14:48:07 +0200

    remove flush

--- streamer/rust/proxy.rs
+++ streamer/rust/proxy.rs
@@ -7,27 +7,24 @@ pub fn forward(mut req: String, mut streamer: BufWriter<TcpStream>) {
     req.push_str("\r\n\r\n");
     match TcpStream::connect(env!("STORE_ADDR")) {
         Ok(mut store) => match store.write_all(req.as_bytes()) {
-            Ok(_) => match store.flush() {
-                Ok(_) => {
-                    let mut buf: Vec<u8> = vec![0; streamer.capacity()];
-                    loop {
-                        match store.read(&mut buf) {
-                            Ok(size) => {
-                                if size == 0 {
-                                    break;
-                                }
-                                match streamer.write_all(&buf[..size]) {
-                                    Ok(_) => (),
-                                    Err(_) => break,
-                                }
+            Ok(_) => {
+                let mut buf: Vec<u8> = vec![0; streamer.capacity()];
+                loop {
+                    match store.read(&mut buf) {
+                        Ok(size) => {
+                            if size == 0 {
+                                break;
                             }
-                            Err(_) => break,
+                            match streamer.write_all(&buf[..size]) {
+                                Ok(_) => (),
+                                Err(_) => break,
+                            }
                         }
+                        Err(_) => break,
                     }
-                    return;
                 }
-                _ => (),
-            },
+                return;
+            }
             _ => (),
         },
         Err(_) => match streamer.write_all(err_codes::ERR_404.as_bytes()) {
