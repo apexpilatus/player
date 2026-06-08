9a3fe40b7b04fffe8f74e9d6cb31366529a0704b
Max <apexpilatus@gmail.com>
2026-06-08 11:53:24 +0200

    update draft for play

--- streamer/rust/player.rs
+++ streamer/rust/player.rs
@@ -46,51 +46,74 @@ pub fn play(params: Option<&str>, mut streamer: BufWriter<TcpStream>) {
     if let Some(params) = params {
         let params = parse_params(params);
         if let Some(mut params) = params {
-            let mut buf: Vec<u8> = vec![0; streamer.capacity()];
-            'get_tracks: loop {
-                let req = format!(
-                    "GET /fetch?album={}&track={} HTTP/1.1\r\n\r\n",
-                    params.album, params.track
+            if let Ok(mut child) = Command::new("play")
+                .env("PATH", env!("STREAMER_PATH"))
+                .stdin(Stdio::piped())
+                //.stderr(Stdio::null())
+                //.stdout(Stdio::piped())
+                .current_dir(env!("STREAMER_PATH"))
+                .spawn()
+            {
+                let mut buf: Vec<u8> = vec![0; streamer.capacity()];
+                let hdr = format!(
+                    "\
+HTTP/1.1 200 OK\r\n\
+Content-Type: text/html; charset=utf-8\r\n\
+Cache-control: no-cache\r\n\
+X-Content-Type-Options: nosniff\r\n\r\n"
                 );
-                if let Ok(mut store) = TcpStream::connect(env!("STORE_ADDR")) {
-                    match store.write_all(req.as_bytes()) {
-                        Ok(_) => {
-                            let reader = BufReader::new(&store);
-                            let mut resp: Vec<String> = Vec::new();
-                            for line in reader.lines() {
-                                match line {
-                                    Ok(line) => {
-                                        if line.is_empty() {
-                                            break;
+                'get_tracks: loop {
+                    let req = format!(
+                        "GET /fetch?album={}&track={} HTTP/1.1\r\n\r\n",
+                        params.album, params.track
+                    );
+                    if let Ok(mut store) = TcpStream::connect(env!("STORE_ADDR")) {
+                        match store.write_all(req.as_bytes()) {
+                            Ok(_) => {
+                                let reader = BufReader::new(&store);
+                                let mut resp: Vec<String> = Vec::new();
+                                for line in reader.lines() {
+                                    match line {
+                                        Ok(line) => {
+                                            if line.is_empty() {
+                                                break;
+                                            }
+                                            resp.push(line);
                                         }
-                                        resp.push(line);
-                                    }
-                                    Err(_) => {
-                                        println!("err read");
-                                        break 'get_tracks;
+                                        Err(_) => {
+                                            println!("err read");
+                                            break 'get_tracks;
+                                        }
                                     }
                                 }
+                                if !(resp.is_empty() || resp[0].contains("404 shit happens")) {
+                                    println!("{}", resp.join("\r\n"));
+                                } else {
+                                    println!("empty or shit");
+                                    break;
+                                }
                             }
-                            if !(resp.is_empty() || resp[0].contains("404 shit happens")) {
-                                println!("{}", resp.join("\r\n"));
-                            } else {
-                                println!("empty or shit");
+                            Err(_) => {
+                                println!("err write");
                                 break;
                             }
                         }
-                        Err(_) => {
-                            println!("err write");
-                            break;
-                        }
+                    } else {
+                        println!("cannot connect");
+                        break;
                     }
-                } else {
-                    println!("cannot connect");
-                    break;
+                    params.track += 1;
                 }
-                params.track += 1;
+                match child.wait() {
+                    _ => return,
+                }
             }
         }
     }
+    match streamer.write_all(err_codes::ERR_404.as_bytes()) {
+        _ => (),
+    }
+
     // if let Ok(mut store) = TcpStream::connect(env!("STORE_ADDR")) {
     //     if let Ok(mut child) = Command::new("play")
     //         .env("PATH", env!("STREAMER_PATH"))
