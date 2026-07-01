a6ce5cf12fe09a3f95ec1aaa5630b6794c41acf6
Max <apexpilatus@gmail.com>
2026-07-01 10:36:01 +0300

    refactor

--- streamer/c/getvolume.c
+++ streamer/c/getvolume.c
@@ -12,29 +12,29 @@ int get_cards() {
 
 int get_volume(char *card_id) {
   snd_mixer_t *mxr;
+  int card_index;
   char *card_name;
   snd_mixer_elem_t *melem;
   long min_vol;
   long max_vol;
   long curr_vol;
-  if (!(snd_mixer_open(&mxr, 0) || snd_mixer_selem_register(mxr, NULL, NULL))) {
-    if (snd_card_get_index(card_id + 3) >= 0 &&
-        !snd_card_get_name(snd_card_get_index(card_id + 3), &card_name)) {
-      if (!(snd_mixer_attach(mxr, card_id) || snd_mixer_load(mxr))) {
-        melem = snd_mixer_first_elem(mxr);
-        for (; melem && !snd_mixer_selem_has_playback_volume_joined(melem);
-             melem = snd_mixer_elem_next(melem))
-          ;
-        if (melem && !(snd_mixer_selem_get_playback_volume_range(
-                           melem, &min_vol, &max_vol) ||
-                       snd_mixer_selem_get_playback_volume(
-                           melem, SND_MIXER_SCHN_UNKNOWN, &curr_vol))) {
-          printf("HTTP/1.1 200 %ld;%ld;%ld\r\n%s%lu\r\n%s\r\n\r\n%s", min_vol,
-                 curr_vol, max_vol, "Content-Length: ", strlen(card_name),
-                 "Content-Type: text/plain", card_name);
-          return 0;
-        }
-      }
+  if (snd_mixer_open(&mxr, 0) == 0 &&
+      snd_mixer_selem_register(mxr, NULL, NULL) == 0 &&
+      (card_index = snd_card_get_index(card_id + 3)) >= 0 &&
+      snd_card_get_name(card_index, &card_name) == 0 &&
+      snd_mixer_attach(mxr, card_id) == 0 && snd_mixer_load(mxr) == 0) {
+    melem = snd_mixer_first_elem(mxr);
+    for (; melem && !snd_mixer_selem_has_playback_volume_joined(melem);
+         melem = snd_mixer_elem_next(melem))
+      ;
+    if (melem && !(snd_mixer_selem_get_playback_volume_range(melem, &min_vol,
+                                                             &max_vol) ||
+                   snd_mixer_selem_get_playback_volume(
+                       melem, SND_MIXER_SCHN_UNKNOWN, &curr_vol))) {
+      printf("HTTP/1.1 200 %ld;%ld;%ld\r\n%s%lu\r\n%s\r\n\r\n%s", min_vol,
+             curr_vol, max_vol, "Content-Length: ", strlen(card_name),
+             "Content-Type: text/plain", card_name);
+      return 0;
     }
   }
   return 1;
