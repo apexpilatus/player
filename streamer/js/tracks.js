44d52be01ebbd04808dc4fd378388723b3c8367b
Max Otrishko <35628650+apexpilatus@users.noreply.github.com>
2026-09-26 16:47:52 +0200

    js

--- streamer/js/tracks.js
+++ streamer/js/tracks.js
@@ -24,18 +24,6 @@ function getmeta(album) {
             resp.text().then(tracks => gettracks(album, tracks.split("\r\n"), 0));
         }
     });
-    // fetch(location.origin + "/meta?album=" + album + "&meta=TITLE=&track=" + track).then(resp => {
-    //     if (resp.status == 200) {
-    //         addEventListener('wheel', (event) => {
-    //             event.preventDefault();
-    //             document.documentElement.scrollLeft += event.deltaY;
-    //         }, { passive: false });
-    //         gettracks(album);
-    //     } else if (track < 100) {
-    //         track++;
-    //         getmeta(album);
-    //     }
-    // });
 }
 
 function gettracks(album, tracks, track) {
